5888fd5b4035088affb9bedad74612ecd547a473
danielreichler <dr@reichler.online>
2020-04-11 14:38:16 +0200

    frontend: fix redirect to login before token loaded or when token is null

--- frontend/src/app/interceptors/auth-interceptor.ts
+++ frontend/src/app/interceptors/auth-interceptor.ts
@@ -1,9 +1,6 @@
 import {Injectable} from '@angular/core';
 import {HttpInterceptor, HttpRequest, HttpHandler} from '@angular/common/http';
 import {TokenService} from '../services/token.service';
-import { environment } from './../../environments/environment';
-
-const EXCLUDED_URLS = ['/login'];
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
@@ -12,10 +9,6 @@ export class AuthInterceptor implements HttpInterceptor {
   }
 
   intercept(request: HttpRequest<any>, next: HttpHandler) {
-    if (EXCLUDED_URLS.includes(request.url.substr(environment.api.baseUrl.length))) {
-      return next.handle(request.clone());
-    }
-
     const authToken = this.tokenService.token;
 
     if (authToken) {
