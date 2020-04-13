2c41e20e880ff5191a59cafeee051df8e19c01ae
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-04-14 00:27:35 +0200

    Added previously missing fields on ContactPersonDto.

--- backend/src/main/java/quarano/tracking/web/ContactPersonDto.java
+++ backend/src/main/java/quarano/tracking/web/ContactPersonDto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import quarano.tracking.ContactPerson.ContactPersonIdentifier;
 import quarano.tracking.EmailAddress;
 import quarano.tracking.PhoneNumber;
+import quarano.tracking.ZipCode;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
@@ -16,15 +17,19 @@ import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @Data
-public class ContactPersonDto {
+class ContactPersonDto {
 
 	@Setter(onMethod = @__(@JsonIgnore)) //
 	@Getter(onMethod = @__(@JsonProperty)) //
 	@JsonSerialize(using = ToStringSerializer.class) //
 	private ContactPersonIdentifier id;
 
-	private @NotEmpty String lastName, firstName;
+	private @NotEmpty String lastName, firstName, street;
+	private String houseNumber;
+	private @NotEmpty String city;
+	private @NotEmpty @Pattern(regexp = ZipCode.PATTERN) String zipCode;
 	private @Pattern(regexp = PhoneNumber.PATTERN) String phone;
+	private @Pattern(regexp = PhoneNumber.PATTERN) String mobilePhone;
 	private @Pattern(regexp = EmailAddress.PATTERN) String email;
 	private String remark;
 	private String identificationHint;
