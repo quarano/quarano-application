e958fa97030c7200854ad92b963314beda975b51
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-04-14 20:03:24 +0200

    Fixed ignoring ContactPersonDto's id property on binding.
    Switched to UUID for rendering and now properly ignore the field's value on deserialization.

--- backend/src/main/java/quarano/tracking/web/ContactPersonDto.java
+++ backend/src/main/java/quarano/tracking/web/ContactPersonDto.java
@@ -2,27 +2,22 @@ package quarano.tracking.web;
 
 import lombok.Data;
 import lombok.Getter;
-import lombok.Setter;
-import quarano.tracking.ContactPerson.ContactPersonIdentifier;
 import quarano.tracking.EmailAddress;
 import quarano.tracking.PhoneNumber;
 import quarano.tracking.ZipCode;
 
+import java.util.UUID;
+
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
-import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
 
 @Data
 class ContactPersonDto {
 
-	@Setter(onMethod = @__(@JsonIgnore)) //
-	@Getter(onMethod = @__(@JsonProperty)) //
-	@JsonSerialize(using = ToStringSerializer.class) //
-	private ContactPersonIdentifier id;
+	@Getter(onMethod = @__(@JsonProperty(access = JsonProperty.Access.READ_ONLY))) //
+	private UUID id;
 
 	private @NotEmpty String lastName, firstName, street;
 	private String houseNumber;
---
+++ backend/src/test/java/quarano/tracking/web/JacksonMarshallingIntegrationTests.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2020 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package quarano.tracking.web;
+
+import static org.assertj.core.api.Assertions.*;
+
+import de.wevsvirushackathon.coronareport.CoronareportBackendApplication;
+import quarano.Quarano;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+
+/**
+ * @author Oliver Drotbohm
+ */
+@SpringBootTest(classes = { Quarano.class, CoronareportBackendApplication.class })
+@ActiveProfiles("integrationtest")
+public class JacksonMarshallingIntegrationTests {
+
+	@Autowired ObjectMapper mapper;
+
+	@Test
+	void rendersIdentifierForContactPersonDtoButDoesNotBindIt() throws Exception {
+
+		var dto = new ContactPersonDto();
+		dto.setId(UUID.randomUUID());
+
+		var result = mapper.writeValueAsString(dto);
+
+		assertThat(JsonPath.parse(result).read("$.id", String.class)).isNotEmpty();
+
+		dto = mapper.readValue(result, ContactPersonDto.class);
+
+		assertThat(dto.getId()).isNull();
+	}
+}
