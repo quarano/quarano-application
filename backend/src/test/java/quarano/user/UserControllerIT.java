81ca00217412d27a3ef76ab4c917f42aae58b270
Oliver Drotbohm <odrotbohm@pivotal.io>
2020-04-15 13:52:15 +0200

    Fixed UserControllerIT.

--- backend/src/test/java/quarano/user/UserControllerIT.java
+++ backend/src/test/java/quarano/user/UserControllerIT.java
@@ -1,13 +1,16 @@
 package quarano.user;
 
-import static de.wevsvirushackathon.coronareport.TestUtil.fromJson;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.options;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static de.wevsvirushackathon.coronareport.TestUtil.*;
+import static org.assertj.core.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.junit.jupiter.api.Assertions;
+import de.wevsvirushackathon.coronareport.CoronareportBackendApplication;
+import de.wevsvirushackathon.coronareport.client.TokenResponse;
+import quarano.Quarano;
+
+import java.util.Map;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -18,104 +21,77 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
-import de.wevsvirushackathon.coronareport.CoronareportBackendApplication;
-import de.wevsvirushackathon.coronareport.client.ClientDto;
-import de.wevsvirushackathon.coronareport.client.TokenResponse;
-import quarano.Quarano;
-import quarano.tracking.TrackedPerson;
-import quarano.tracking.TrackedPersonDataInitializer;
-import quarano.tracking.web.TrackedPersonDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK, classes = {CoronareportBackendApplication.class, Quarano.class})
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = { CoronareportBackendApplication.class, Quarano.class })
 @AutoConfigureMockMvc
 @ActiveProfiles("integrationtest")
-public class UserControllerIT {
+class UserControllerIT {
 
-	@Autowired
-	private MockMvc mvc;
-
+	@Autowired MockMvc mvc;
+	@Autowired ObjectMapper mapper;
 
 	@Test
-	public void testLoginWithValidCredentials() throws Exception {
+	void testLoginWithValidCredentials() throws Exception {
 
-		// Accounts and password created by dummy data input beans
-		// given
-		final String username = "DemoAccount";
-		final String password = "DemoPassword";
-		
-		final TrackedPerson person = TrackedPersonDataInitializer.INDEX_PERSON2_IN_ENROLLMENT;
-		final TrackedPersonDto expectedPersonDto = TrackedPersonDto.builder()
-				.firstName(person.getFirstName())
-				.lastName(person.getLastName())
-				.infected(true)
-				.email(person.getEmailAddress().toString())
-				.phone(person.getPhoneNumber().toString())
-				.build();
-		
-
-		String requestbody = createLoginRequestBody(username, password);
+		// Accounts and password created by dummy data input beans given
+		var username = "DemoAccount";
+		var password = "DemoPassword";
 
 		// when
-		String resultLogin = mvc
-				.perform(post("/login").header("Origin", "*").contentType(MediaType.APPLICATION_JSON)
-						.content(requestbody))
-
-				.andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+		String resultLogin = mvc.perform(post("/login") //
+				.header("Origin", "*") //
+				.contentType(MediaType.APPLICATION_JSON) //
+				.content(createLoginRequestBody(username, password))) //
+				.andExpect(status().isOk()) //
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)) //
 				.andReturn().getResponse().getContentAsString();
 		TokenResponse response = fromJson(resultLogin, TokenResponse.class);
 
 		// check if token is valid for authentication
-		String resultDtoStr = mvc
-				.perform(
-						get("/user/me").header("Origin", "*").header("Authorization", "Bearer " + response.getToken())
-								.contentType(MediaType.APPLICATION_JSON))
-				.andExpect(status().isOk()).andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+		String resultDtoStr = mvc.perform(get("/api/user/me") //
+				.header("Origin", "*") //
+				.header("Authorization", "Bearer " + response.getToken()) //
+				.contentType(MediaType.APPLICATION_JSON)) //
+				.andExpect(status().isOk()) //
+				.andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON)) //
 				.andReturn().getResponse().getContentAsString();
-		UserDto resultDto = fromJson(resultDtoStr, UserDto.class);
 
-		// then
-		Assertions.assertEquals(expectedPersonDto, resultDto.getClient());
+		DocumentContext document = JsonPath.parse(resultDtoStr);
 
+		assertThat(document.read("$.username", String.class)).isEqualTo("DemoAccount");
+		assertThat(document.read("$.firstName", String.class)).isEqualTo("Markus");
+		assertThat(document.read("$.lastName", String.class)).isEqualTo("Hanser");
 	}
 
 	@Test
-	public void testLoginWithInvalidCredentials() throws Exception {
+	void testLoginWithInvalidCredentials() throws Exception {
 
-		// Accounts and password created by dummy data input beans
-		// given
-		final String username = "DemoAccount";
-		final String password = "My-Wrong-Password";
-
-		String requestbody = createLoginRequestBody(username, password);
+		// Accounts and password created by dummy data input beans given
+		var username = "DemoAccount";
+		var password = "My-Wrong-Password";
 
 		// when
-		mvc.perform(post("/login")
-					.header("Origin", "*")
-					.contentType(MediaType.APPLICATION_JSON)
-					.content(requestbody))
-
-			.andExpect(status().isUnauthorized())
-			.andReturn().getResponse()
-			.getContentAsString();
-
+		mvc.perform(post("/login") //
+				.header("Origin", "*") //
+				.contentType(MediaType.APPLICATION_JSON) //
+				.content(createLoginRequestBody(username, password))).andExpect(status().isUnauthorized());
 	}
 
 	@Test
-	public void testThatPreflightRequestsGetNotBlocked() throws Exception {
+	void testThatPreflightRequestsGetNotBlocked() throws Exception {
 
 		// check if token is valid for authentication
-		mvc.perform(options("/client/me")
-				.header("Origin", "*")
-				.header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpMethod.OPTIONS))
-				.andExpect(status().isOk()).andReturn().getResponse()
-				.getContentAsString();
-
+		mvc.perform(options("/client/me") //
+				.header("Origin", "*") //
+				.header(HttpHeaders.ACCESS_CONTROL_REQUEST_METHOD, HttpMethod.OPTIONS)) //
+				.andExpect(status().isOk());
 	}
 
-	public static String createLoginRequestBody(String username, String password) {
-		return "{ \"username\": \"" + username + "\", \"password\": \"" + password + "\" }";
+	public String createLoginRequestBody(String username, String password) throws Exception {
+		return mapper.writeValueAsString(Map.of("username", username, "password", password));
 	}
-
-
-
 }
