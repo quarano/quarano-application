faa991ea23ebe9f67fa75e60f8eae5f2980b98f7
Felix Schultze <felix.schultze@bridging-it.de>
2020-04-25 16:05:28 +0200

    remove not needed imports

--- backend/src/main/java/quarano/tracking/ZipCode.java
+++ backend/src/main/java/quarano/tracking/ZipCode.java
@@ -20,23 +20,9 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
-import java.lang.annotation.Documented;
-import java.lang.annotation.Retention;
-import java.lang.annotation.Target;
-import java.util.regex.Pattern;
-
 import javax.persistence.Column;
 import javax.persistence.Embeddable;
-import javax.validation.Constraint;
-import javax.validation.Payload;
-
-import static java.lang.annotation.ElementType.ANNOTATION_TYPE;
-import static java.lang.annotation.ElementType.CONSTRUCTOR;
-import static java.lang.annotation.ElementType.FIELD;
-import static java.lang.annotation.ElementType.METHOD;
-import static java.lang.annotation.ElementType.PARAMETER;
-import static java.lang.annotation.ElementType.TYPE_USE;
-import static java.lang.annotation.RetentionPolicy.RUNTIME;
+import java.util.regex.Pattern;
 
 /**
  * @author Oliver Drotbohm
