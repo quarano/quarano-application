2ecefa161c5f152d4408a5d926faa82b6142863f
Felix Schultze <felix.schultze@bridging-it.de>
2020-04-26 10:20:18 +0200

    CORE-88 - Add validations to TrackedCaseDto and SymptomDto.

--- backend/src/main/java/quarano/reference/SymptomDto.java
+++ backend/src/main/java/quarano/reference/SymptomDto.java
@@ -1,15 +1,15 @@
 package quarano.reference;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import quarano.core.validation.Alphabetic;
 
 import java.util.UUID;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fasterxml.jackson.annotation.JsonProperty;
 
 @Data
 @NoArgsConstructor
@@ -20,6 +20,6 @@ public class SymptomDto {
 	@Setter(onMethod = @__(@JsonIgnore)) //
 	private UUID id;
 
-	private String name;
+	private @Alphabetic	String name;
 	private boolean isCharacteristic;
 }
--- backend/src/test/java/quarano/department/web/TrackedCaseControllerWebIntegrationTests.java
+++ backend/src/test/java/quarano/department/web/TrackedCaseControllerWebIntegrationTests.java
@@ -20,10 +20,19 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.header;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jayway.jsonpath.JsonPath;
+import com.jayway.jsonpath.ReadContext;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 import quarano.QuaranoWebIntegrationTest;
 import quarano.ValidationUtils;
 import quarano.WithQuaranoUser;
@@ -105,6 +114,24 @@ class TrackedCaseControllerWebIntegrationTests {
 				.forEach(it -> {
 					assertThat(response.read("$." + it, String.class)).isNotNull();
 				});
+	}
+
+	@Test
+	void rejectsCaseCreationWithFirstNameLastNameAndCityContainingInvalidCharacters() throws Exception {
+		var today = LocalDate.now();
+		var payload = new TrackedCaseDto() //
+				.setFirstName("Michael 123") //
+				.setLastName("Mustermann 123") //
+				.setTestDate(today) //
+				.setQuarantineStartDate(today) //
+				.setQuarantineEndDate(today.plus(configuration.getQuarantinePeriod())) //
+				.setPhone("0123456789")
+				.setCity("city 123");
+		var document = expectBadRequest(HttpMethod.POST, "/api/hd/cases", payload);
+
+		assertThat(document.read("$.firstName", String.class)).isEqualTo("Dieses Feld darf nur Buchstaben enthalten!");
+		assertThat(document.read("$.lastName", String.class)).isEqualTo("Dieses Feld darf nur Buchstaben enthalten!");
+		assertThat(document.read("$.city", String.class)).isEqualTo("Dieses Feld darf nur Buchstaben enthalten!");
 	}
 
 	@Test
