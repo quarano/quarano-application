152b24f780ab92ec784a9d3dfa192a500a0d2216
Mira Manger <mira.manger@gmx.de>
2020-04-26 10:39:36 +0200

    mark alerts as edited

--- frontend/src/app/utils/date-extensions.ts
+++ frontend/src/app/utils/date-extensions.ts
@@ -2,6 +2,7 @@ interface Date {
   addDays(numberOfDays: number): Date;
   getDateWithoutTime(): string;
   toCustomLocaleDateString(): string;
+  isDateInPast(): boolean;
 }
 
 Date.prototype.addDays = function (numberOfDays) {
@@ -19,3 +20,9 @@ Date.prototype.toCustomLocaleDateString = function () {
   const date = new Date(this.valueOf());
   return date.toLocaleDateString('de-DE', { day: '2-digit', month: '2-digit', year: 'numeric' });
 };
+
+Date.prototype.isDateInPast = function () {
+  const date = new Date(this.valueOf());
+  const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
+  return today.getDateWithoutTime() > date.getDateWithoutTime();
+}
