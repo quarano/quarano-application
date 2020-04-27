e07a47b7fedeb8e884dcbc3e2db6e04b4378a6ad
Mira Manger <mira.manger@gmx.de>
2020-04-27 20:34:19 +0200

    use CaseType enum in template

--- frontend/src/app/modules/welcome/landing/landing.component.ts
+++ frontend/src/app/modules/welcome/landing/landing.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { ClientType } from '../../../models/report-case';
+import { ClientType } from '@models/report-case';
 
 @Component({
   selector: 'app-landing',
@@ -8,14 +8,14 @@ import { ClientType } from '../../../models/report-case';
   styleUrls: ['./landing.component.scss']
 })
 export class LandingComponent implements OnInit {
-
-  public userType = ClientType.index.valueOf();
+  public ClientType = ClientType;
+  public userType = ClientType.Index;
   public clientcode: string;
 
   constructor(private router: Router, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.userType = this.route.snapshot.paramMap.get('usertype') || this.userType;
+    this.userType = this.route.snapshot.paramMap.get('usertype') as ClientType || this.userType;
     this.clientcode = this.route.snapshot.paramMap.get('clientcode');
   }
 }
