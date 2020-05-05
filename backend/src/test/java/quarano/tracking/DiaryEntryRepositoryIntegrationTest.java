d90f753693a1444a606799a605ccc049edf7ce82
Felix Schultze <felix.schultze@bridging-it.de>
2020-05-06 00:23:57 +0200

    findMissingDiaryEntries: refactor tests to be more precise

--- backend/src/test/java/quarano/tracking/DiaryEntryRepositoryIntegrationTest.java
+++ backend/src/test/java/quarano/tracking/DiaryEntryRepositoryIntegrationTest.java
@@ -1,16 +1,14 @@
 package quarano.tracking;
 
-import static org.assertj.core.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 import lombok.RequiredArgsConstructor;
 import quarano.QuaranoIntegrationTest;
+import quarano.tracking.TrackedPerson.TrackedPersonIdentifier;
 
 import java.util.List;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-@Disabled
 @QuaranoIntegrationTest
 @RequiredArgsConstructor
 class DiaryEntryRepositoryIntegrationTest {
@@ -18,26 +16,28 @@ class DiaryEntryRepositoryIntegrationTest {
 	private final DiaryEntryRepository diaries;
 
 	@Test
-	void testFindMissingDiaryEntryPersonsForTwoSlots() {
+	void testFindMissingDiaryEntryPersonsForLastOneSlot() {
+
+		Slot now = Slot.now();
+
+		var diaryEntries = diaries.findMissingDiaryEntryPersons(List.of(now));
+
+		List<TrackedPersonIdentifier> personIds = diaryEntries.toList();
+		assertThat(personIds).containsExactly(TrackedPersonDataInitializer.createSandra().getId(),
+				TrackedPersonDataInitializer.createNadine().getId(), TrackedPersonDataInitializer.createSiggi().getId(),
+				TrackedPersonDataInitializer.createGustav().getId());
+	}
+
+	@Test
+	void testFindMissingDiaryEntryPersonsForLastTwoSlots() {
 
 		Slot now = Slot.now();
 		Slot previous = now.previous();
 
 		var diaryEntries = diaries.findMissingDiaryEntryPersons(List.of(now, previous));
 
-		int size = diaryEntries.toList().size();
-		assertThat(size).isEqualTo(13);
-	}
-
-	@Test
-	void testFindMissingDiaryEntryPersonsForThreeSlots() {
-
-		Slot now = Slot.now();
-		Slot previous = now.previous();
-
-		var diaryEntries = diaries.findMissingDiaryEntryPersons(List.of(now, previous, previous.previous()));
-
-		int size = diaryEntries.toList().size();
-		assertThat(size).isEqualTo(10);
+		List<TrackedPersonIdentifier> personIds = diaryEntries.toList();
+		assertThat(personIds).containsExactly(TrackedPersonDataInitializer.createSandra().getId(),
+				TrackedPersonDataInitializer.createSiggi().getId(), TrackedPersonDataInitializer.createGustav().getId());
 	}
 }
