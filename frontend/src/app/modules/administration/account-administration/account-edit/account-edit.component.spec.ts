f18e5acffef05be6e3a011156322722ead5686df
Mira Manger <mira.manger@gmx.de>
2020-05-05 22:53:37 +0200

    test fixes

--- frontend/src/app/modules/administration/account-administration/account-edit/account-edit.component.spec.ts
+++ frontend/src/app/modules/administration/account-administration/account-edit/account-edit.component.spec.ts
@@ -1,23 +1,51 @@
+import { FormBuilder } from '@angular/forms';
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { UserEditComponent } from './user-edit.component';
+import { AccountEditComponent } from './account-edit.component';
+import { RouterTestingModule } from '@angular/router/testing';
+import { ApiService } from '@services/api.service';
+import { SnackbarService } from '@services/snackbar.service';
+import { ActivatedRoute } from '@angular/router';
+import { of } from 'rxjs';
 
-describe('UserEditComponent', () => {
-  let component: UserEditComponent;
-  let fixture: ComponentFixture<UserEditComponent>;
+describe('AccountEditComponent', () => {
+  let component: AccountEditComponent;
+  let fixture: ComponentFixture<AccountEditComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ UserEditComponent ]
+      imports: [RouterTestingModule],
+      declarations: [AccountEditComponent],
+      providers: [
+        FormBuilder,
+        { provide: ApiService, useValue: jasmine.createSpyObj(['createDiaryEntry']) },
+        { provide: SnackbarService, useValue: jasmine.createSpyObj(['warning', 'success']) },
+        {
+          provide: ActivatedRoute, useValue: {
+            data: of({
+              account: {
+                accountId: null,
+                firstName: null,
+                lastName: null,
+                username: null,
+                _links: null,
+                email: null,
+                roles: []
+              },
+            }),
+            snapshot: { paramMap: { get: (value) => '' } }
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(UserEditComponent);
+    fixture = TestBed.createComponent(AccountEditComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
