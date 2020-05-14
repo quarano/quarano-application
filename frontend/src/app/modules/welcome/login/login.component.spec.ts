cf9a9e9a99656b0a8612896c3683c69bf4f0a277
Mira Manger <mira.manger@gmx.de>
2020-05-14 23:43:12 +0200

    added HttpClientTestingModule to testbed config for login component

--- frontend/src/app/modules/welcome/login/login.component.spec.ts
+++ frontend/src/app/modules/welcome/login/login.component.spec.ts
@@ -3,9 +3,9 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { LoginComponent } from './login.component';
 import { SnackbarService } from '@services/snackbar.service';
 import { UserService } from '@services/user.service';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {Router} from '@angular/router';
-import {of} from 'rxjs';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -15,6 +15,7 @@ describe('LoginComponent', () => {
     const userService = jasmine.createSpyObj(['login', 'isHealthDepartmentUser']);
     userService.isLoggedIn$ = of();
     TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
       providers: [
         { provide: UserService, useValue: userService },
         { provide: SnackbarService, useValue: jasmine.createSpyObj(['success']) },
