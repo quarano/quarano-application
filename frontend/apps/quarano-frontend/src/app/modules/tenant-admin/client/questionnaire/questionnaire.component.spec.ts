be20b716af3fd48e233f42df763dfbab16ecdeb0
Gregor Speck <gregor.speck@bridging-it.de>
2020-05-23 21:34:54 +0200

    added 'questionnaire' tab component

---
+++ frontend/apps/quarano-frontend/src/app/modules/tenant-admin/client/questionnaire/questionnaire.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { QuestionnaireComponent } from './questionnaire.component';
+
+describe('QuestionnaireComponent', () => {
+  let component: QuestionnaireComponent;
+  let fixture: ComponentFixture<QuestionnaireComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ QuestionnaireComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(QuestionnaireComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
