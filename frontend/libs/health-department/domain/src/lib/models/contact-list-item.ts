31a8788cc9c61cb93bcd5260baf66c416d63289f
VILLAN3LL3 <julidarockt@gmail.com>
2020-06-20 19:22:26 +0200

    CORE-119 renamed contactDates

--- frontend/libs/health-department/domain/src/lib/models/contact-list-item.ts
+++ frontend/libs/health-department/domain/src/lib/models/contact-list-item.ts
@@ -2,7 +2,7 @@ import { HalResponse } from '@qro/shared/util-data-access';
 import { CaseStatus } from './case-detail';
 
 export interface ContactListItemDto extends HalResponse {
-  descendingSortedContactDates: Date[];
+  contactDates: Date[];
   contactStatus: CaseStatus;
   lastName: string;
   firstName: string;
