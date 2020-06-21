d0cd564fafbabb90242f90b105a2b689f992cf24
Felix Schultze <felix.schultze@bridging-it.de>
2020-06-21 09:25:09 +0200

    CORE-453: Execution of E2E Tests in GitHub
    In general the run environment and the needed commands are nearly the same as in local dev env.
    So they are triggered by simply running "npm run e2e".
    To support that, we setup the environment with a german locale and timezone and start the backend
    before running those e2e tests.
    Afterwards the backend process is killed by selecting the correct process by its listening port 8080.
    With that setup, we do NOT have to provide any CI-specific host variables, proxy settings, deployed backends or others.

--- frontend/apps/quarano-frontend-e2e/src/integration/client/diary.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/client/diary.spec.ts
@@ -8,64 +8,62 @@ describe('Adding new symptom to symptom diary', () => {
   });
 
   it('not possible, when no body temperature is selected.', () => {
-    cy.location("pathname").should('include', 'client/diary/diary-list');
+    cy.location('pathname').should('include', 'client/diary/diary-list');
     cy.get('[data-cy="add-diary-entry"]').click();
-    cy.get('[data-cy="body-temperature"]').should("exist");
-    cy.get('[data-cy="symptoms-select"]').should("exist");
-    cy.get('[data-cy="other-symptoms"]').should("exist");
-    cy.get('[data-cy="other-contacts"]').should("exist");
-    cy.get('[data-cy="add-missing-contacts"]').should("exist");
+    cy.get('[data-cy="body-temperature"]').should('exist');
+    cy.get('[data-cy="symptoms-select"]').should('exist');
+    cy.get('[data-cy="other-symptoms"]').should('exist');
+    cy.get('[data-cy="other-contacts"]').should('exist');
+    cy.get('[data-cy="add-missing-contacts"]').should('exist');
 
-    cy.get('[data-cy="save-diary-entry"] button').should("be.disabled");
-    cy.get('[data-cy="cancel-diary-entry"]').should("be.enabled");
-
+    cy.get('[data-cy="save-diary-entry"] button').should('be.disabled');
+    cy.get('[data-cy="cancel-diary-entry"]').should('be.enabled');
   });
 
   it('happy path', () => {
-    cy.location("pathname").should('include', 'client/diary/diary-list');
+    cy.location('pathname').should('include', 'client/diary/diary-list');
     cy.get('[data-cy="add-diary-entry"]').click();
-    cy.get('[data-cy="body-temperature"]').should("exist");
-    cy.get('[data-cy="symptoms-select"]').should("exist");
-    cy.get('[data-cy="other-symptoms"]').should("exist");
-    cy.get('[data-cy="other-contacts"]').should("exist");
-    cy.get('[data-cy="add-missing-contacts"]').should("exist");
-    cy.get('[data-cy="save-diary-entry"] button').should("be.disabled");
+    cy.get('[data-cy="body-temperature"]').should('exist');
+    cy.get('[data-cy="symptoms-select"]').should('exist');
+    cy.get('[data-cy="other-symptoms"]').should('exist');
+    cy.get('[data-cy="other-contacts"]').should('exist');
+    cy.get('[data-cy="add-missing-contacts"]').should('exist');
+    cy.get('[data-cy="save-diary-entry"] button').should('be.disabled');
 
-    cy.get('[data-cy="body-temperature"]')
-      .trigger("mousedown", {button: 0});
+    cy.get('[data-cy="body-temperature"]').trigger('mousedown', { button: 0 });
 
     cy.get('mat-slide-toggle').should('have.length', 8);
-    cy.get('#mat-slide-toggle-1').should("exist");
-    cy.get('#mat-slide-toggle-6').should("exist");
+    cy.get('#mat-slide-toggle-1').should('exist');
+    cy.get('#mat-slide-toggle-6').should('exist');
     cy.get('#mat-slide-toggle-1').click();
     cy.get('#mat-slide-toggle-6').click();
 
-    cy.get('[data-cy="other-symptoms"] input').type("Ageusie");
+    cy.get('[data-cy="other-symptoms"] input').type('Ageusie');
     cy.get('mat-option').click();
-    cy.get('[data-cy="other-symptoms"] input').type("Agnosie");
+    cy.get('[data-cy="other-symptoms"] input').type('Agnosie');
     cy.get('mat-option').click();
-    cy.get('[data-cy="other-contacts"] input').type("Lora Laurer");
+    cy.get('[data-cy="other-contacts"] input').type('Lora Laurer');
     cy.get('mat-option').click();
 
-    cy.get('[data-cy="save-diary-entry"] button').should("be.enabled");
-    cy.get('[data-cy="cancel-diary-entry"]').should("be.enabled");
+    cy.get('[data-cy="save-diary-entry"] button').should('be.enabled');
+    cy.get('[data-cy="cancel-diary-entry"]').should('be.enabled');
     cy.get('[data-cy="save-diary-entry"] button').click();
 
     cy.wait('@diary').its('status').should('eq', 201);
 
-    cy.get('@diary').its('request.body').then(body => {
-      const bodyTemperature = body.bodyTemperature;
-      const contacts = body.contacts;
-      const symptoms = body.symptoms;
-      const entryId = body.id;
-      expect(bodyTemperature).to.eq(39.5);
-      expect(contacts).to.have.length(1);
-      expect(symptoms).to.have.length(4);
-      expect(entryId).to.eq(null);
-    });
+    cy.get('@diary')
+      .its('request.body')
+      .then((body) => {
+        const bodyTemperature = body.bodyTemperature;
+        const contacts = body.contacts;
+        const symptoms = body.symptoms;
+        const entryId = body.id;
+        expect(bodyTemperature).to.eq(39.4);
+        expect(contacts).to.have.length(1);
+        expect(symptoms).to.have.length(4);
+        expect(entryId).to.eq(null);
+      });
 
-    cy.location("pathname").should('include', 'client/diary/diary-list');
-
+    cy.location('pathname').should('include', 'client/diary/diary-list');
   });
-
 });
