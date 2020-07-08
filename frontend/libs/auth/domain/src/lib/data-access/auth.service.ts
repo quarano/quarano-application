395aec28466f7964f5f079ad1529b4945e7f44fa
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2020-07-08 19:56:09 +0200

    CORE-192 Setting token fetched from login/registration response's headers (#96)
    Co-authored-by: Jakub Lebiedziński <jakub.lebiedzinski@develocraft.com>

--- frontend/libs/auth/domain/src/lib/data-access/auth.service.ts
+++ frontend/libs/auth/domain/src/lib/data-access/auth.service.ts
@@ -20,14 +20,8 @@ export class AuthService {
     return this.httpClient.get(`${this.apiUrl}/api/registration/checkusername/${username}`);
   }
 
-  login(username: string, password: string): Observable<{ token: string }> {
-    return this.httpClient
-      .post<{ token: string }>(`${this.apiUrl}/login`, { username, password }, { observe: 'response' })
-      .pipe(
-        map((response) => {
-          return { token: response.headers.get('x-auth-token') };
-        })
-      );
+  login(username: string, password: string): Observable<any> {
+    return this.httpClient.post(`${this.apiUrl}/login`, { username, password }, { observe: 'response' });
   }
 
   getMe(): Observable<UserDto> {
