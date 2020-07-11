64f4f0c6213ebbe7f88373dad700e5b5f299ae03
sebastian <clausing.sebastian@gmail.com>
2020-07-11 11:47:37 +0200

    CORE-341 added test provider

--- frontend/libs/health-department/domain/src/lib/store/case.service.spec.ts
+++ frontend/libs/health-department/domain/src/lib/store/case.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CaseService } from './case.service';
+import { EntityCollectionServiceElementsFactory } from '@ngrx/data';
 
 describe('CaseDetailService', () => {
   let service: CaseService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [{ provide: EntityCollectionServiceElementsFactory, useValue: {} }],
+    });
     service = TestBed.inject(CaseService);
   });
 
