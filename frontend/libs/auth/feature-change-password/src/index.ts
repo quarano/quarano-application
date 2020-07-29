861274b882fe1bf40b90b372d2886a5ff1a6752b
Tunafleur <tunafleur@gmail.com>
2020-07-29 20:35:19 +0200

    CORE-334 added login dialog when HD user's password is expired or initial (#101)
    * added login dialog when HD user's password is expired or initial
    
    * fix unit tests, and naming conventions in login component
    
    * changed feature-change-password-component to nx scope "UI"
    
    Co-authored-by: Tuna <extern.cicek_tuna-murat@allianz.de>

--- frontend/libs/auth/feature-change-password/src/index.ts
+++ frontend/libs/auth/feature-change-password/src/index.ts
@@ -1,1 +1,2 @@
 export * from './lib/auth-feature-change-password.module';
+export * from './lib/change-password/change-password.component';
--- frontend/libs/auth/feature-login/src/lib/login/login.component.ts
+++ frontend/libs/auth/feature-login/src/lib/login/login.component.ts
@@ -1,3 +1,4 @@
+import { HttpResponse } from '@angular/common/http';
 import { ValidationErrorGenerator } from '@qro/shared/util-forms';
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
@@ -6,6 +7,8 @@ import { filter, take } from 'rxjs/operators';
 import { MatInput } from '@angular/material/input';
 import { UserService } from '@qro/auth/domain';
 import { SnackbarService } from '@qro/shared/util-snackbar';
+import { ChangePasswordComponent } from '@qro/auth/feature-change-password';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'qro-login',
@@ -21,7 +24,12 @@ export class LoginComponent implements OnInit {
     password: new FormControl(null, Validators.required),
   });
 
-  constructor(private userService: UserService, private snackbarService: SnackbarService, private router: Router) {}
+  constructor(
+    private userService: UserService,
+    private snackbarService: SnackbarService,
+    private router: Router,
+    private matDialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
     this.userService.isLoggedIn$
@@ -39,10 +47,14 @@ export class LoginComponent implements OnInit {
     this.userService
       .login(this.loginFormGroup.controls.username.value, this.loginFormGroup.controls.password.value)
       .subscribe(
-        (_) => {
+        (resData) => {
           this.snackbarService.success('Willkommen bei quarano');
           if (this.userService.isHealthDepartmentUser) {
-            this.router.navigate(['/health-department/index-cases/case-list']);
+            if (this.checkIfPasswordChangeNeeded(resData)) {
+              this.openPasswordChangeDialog();
+            } else {
+              this.router.navigate(['/health-department/index-cases/case-list']);
+            }
           } else {
             this.router.navigate(['/client/diary/diary-list']);
           }
@@ -56,6 +68,37 @@ export class LoginComponent implements OnInit {
         }
       )
       .add(() => (this.loading = false));
+  }
+
+  /**
+   * Checks whether in the _links object there is only one element and if so, if it is
+   * referreing to the password page, then the user needs to change his
+   * initial password
+   * @param response the data received from the backend from the login api Call
+   */
+  checkIfPasswordChangeNeeded(response: HttpResponse<any>): boolean {
+    const resBody = response.body;
+    if (resBody && resBody._links && resBody._links.length > 0) {
+      const changeNeeded = !!resBody._links.find((link) => link.rel === 'change-password');
+      return changeNeeded;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * Opens the Change Password Component with a Material Dialog and
+   * routes the user afterwards if the password change was successful
+   */
+  openPasswordChangeDialog(): void {
+    this.matDialog
+      .open(ChangePasswordComponent, { disableClose: true, data: { mode: 'initialPasswordChange' } })
+      .afterClosed()
+      .subscribe((result) => {
+        if (result === 'success') {
+          this.router.navigate(['/health-department/index-cases/case-list']);
+        }
+      });
   }
 
   trimValue(input: MatInput) {
