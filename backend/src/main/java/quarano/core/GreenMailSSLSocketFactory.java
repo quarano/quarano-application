a39aae79a75f57c1a91a1aa48392450569089822
Jens Kutzsche <github@gebea.de>
2020-08-11 14:23:02 +0200

    CORE-61 + CORE-257 fixes an error with the changed certificate of GreenMail

--- backend/src/main/java/quarano/core/GreenMailSSLSocketFactory.java
+++ backend/src/main/java/quarano/core/GreenMailSSLSocketFactory.java
@@ -5,7 +5,6 @@ import java.net.InetAddress;
 import java.net.Socket;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
-import java.util.Base64;
 
 import javax.net.SocketFactory;
 import javax.net.ssl.SSLContext;
@@ -100,28 +99,34 @@ public class GreenMailSSLSocketFactory extends SSLSocketFactory {
 
 		@Override
 		public void checkServerTrusted(X509Certificate[] cert, String authType) throws CertificateException {
-			var derCert = cert[0].getEncoded();
-			var pemCert = new String(Base64.getEncoder().encode(derCert));
 
-			var trust = pemCert.equals("MIIDgTCCAmmgAwIBAgIEG8kG2jANBgkqhkiG9w0BAQsFADBxMQswCQYDVQQGEwJV" +
-					"UzEeMBwGA1UEChMVSWNlZ3JlZW4gVGVjaG5vbG9naWVzMRIwEAYDVQQLEwlHcmVl" +
-					"bk1haWwxLjAsBgNVBAMTJUdyZWVuTWFpbCBzZWxmc2lnbmVkIFRlc3QgQ2VydGlm" +
-					"aWNhdGUwHhcNMjAwNzE5MjA0MDIwWhcNMjIxMDIyMjA0MDIwWjBxMQswCQYDVQQG" +
-					"EwJVUzEeMBwGA1UEChMVSWNlZ3JlZW4gVGVjaG5vbG9naWVzMRIwEAYDVQQLEwlH" +
-					"cmVlbk1haWwxLjAsBgNVBAMTJUdyZWVuTWFpbCBzZWxmc2lnbmVkIFRlc3QgQ2Vy" +
-					"dGlmaWNhdGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCTd+hNn5St" +
-					"VtsJfWcUq3Q+i+jGWbtEh/LGm8g+qyoxL0YE5gTkoAZ/3qk2hWM1WG+iscPJu79d" +
-					"0BHyPQGB7QHjpBmV3aDJj0AINqoLf74Q9Cs65oLtQ5TKWrmvsMKlzbg2q5g/C0hX" +
-					"KoYBPoHWb/J2a05C06JJjtBxjy3dBcT241nFNUaWw9k8yojp7w9J7a+3rQMgOinc" +
-					"xZSdJbNB6uRE7cnRvJaZtIOKkIRPpikV3/pUqdr/vXGsgLSBZuXCeZ81YNmdVSDw" +
-					"66oJqRVHjEKA7pamDst7kRgOsOZiN8/xjibsUME7tR4xf0SU+ZW7NVBrSIAniB+M" +
-					"Cjc9GGo/ER+tAgMBAAGjITAfMB0GA1UdDgQWBBTlGjYagZ/neX5LQi4YC4DdpCbM" +
-					"OTANBgkqhkiG9w0BAQsFAAOCAQEASrUH+yqWJNoY4PBZMpYkcDqZfLjXdi2lBvX5" +
-					"Czh5GlXQAMKEoQUGQit6CaWg3a3oxOMiDhZPfywSjvl3PxoAExbPOUTjvYtWAIxT" +
-					"2DZpzkr07a/LCu3BkFXlco71pWZBXOP4QfJn5K7W/VYKK4sxbn+3WuprRmTiNOlr" +
-					"sviBuSlOYME5ot814DxxI25yXqD/9mQ7b6a9TLLORBJxKk5JihShPH8xehH6V+Qu" +
-					"25AJTTETw5O//q2NisClVC6UgCyE+Z/fQaVNdAGoXMARQShCOMGUbtuMx1XqHNoR" +
-					"fXg+oj22QabirZg97RYeh3s7lnBRy1QmFPKu5yF9JSyAspvtuw==");
+			var trust = cert[0].getSubjectX500Principal().getName()
+					.equals("CN=GreenMail selfsigned Test Certificate,OU=GreenMail,O=Icegreen Technologies,C=US");
+
+			// The Certificate is regularly renewed.
+			//
+			// var derCert = cert[0].getEncoded();
+			// var pemCert = new String(Base64.getEncoder().encode(derCert));
+			//
+			// var trust = pemCert.equals("MIIDgTCCAmmgAwIBAgIEG8kG2jANBgkqhkiG9w0BAQsFADBxMQswCQYDVQQGEwJV" +
+			// "UzEeMBwGA1UEChMVSWNlZ3JlZW4gVGVjaG5vbG9naWVzMRIwEAYDVQQLEwlHcmVl" +
+			// "bk1haWwxLjAsBgNVBAMTJUdyZWVuTWFpbCBzZWxmc2lnbmVkIFRlc3QgQ2VydGlm" +
+			// "aWNhdGUwHhcNMjAwNzE5MjA0MDIwWhcNMjIxMDIyMjA0MDIwWjBxMQswCQYDVQQG" +
+			// "EwJVUzEeMBwGA1UEChMVSWNlZ3JlZW4gVGVjaG5vbG9naWVzMRIwEAYDVQQLEwlH" +
+			// "cmVlbk1haWwxLjAsBgNVBAMTJUdyZWVuTWFpbCBzZWxmc2lnbmVkIFRlc3QgQ2Vy" +
+			// "dGlmaWNhdGUwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCTd+hNn5St" +
+			// "VtsJfWcUq3Q+i+jGWbtEh/LGm8g+qyoxL0YE5gTkoAZ/3qk2hWM1WG+iscPJu79d" +
+			// "0BHyPQGB7QHjpBmV3aDJj0AINqoLf74Q9Cs65oLtQ5TKWrmvsMKlzbg2q5g/C0hX" +
+			// "KoYBPoHWb/J2a05C06JJjtBxjy3dBcT241nFNUaWw9k8yojp7w9J7a+3rQMgOinc" +
+			// "xZSdJbNB6uRE7cnRvJaZtIOKkIRPpikV3/pUqdr/vXGsgLSBZuXCeZ81YNmdVSDw" +
+			// "66oJqRVHjEKA7pamDst7kRgOsOZiN8/xjibsUME7tR4xf0SU+ZW7NVBrSIAniB+M" +
+			// "Cjc9GGo/ER+tAgMBAAGjITAfMB0GA1UdDgQWBBTlGjYagZ/neX5LQi4YC4DdpCbM" +
+			// "OTANBgkqhkiG9w0BAQsFAAOCAQEASrUH+yqWJNoY4PBZMpYkcDqZfLjXdi2lBvX5" +
+			// "Czh5GlXQAMKEoQUGQit6CaWg3a3oxOMiDhZPfywSjvl3PxoAExbPOUTjvYtWAIxT" +
+			// "2DZpzkr07a/LCu3BkFXlco71pWZBXOP4QfJn5K7W/VYKK4sxbn+3WuprRmTiNOlr" +
+			// "sviBuSlOYME5ot814DxxI25yXqD/9mQ7b6a9TLLORBJxKk5JihShPH8xehH6V+Qu" +
+			// "25AJTTETw5O//q2NisClVC6UgCyE+Z/fQaVNdAGoXMARQShCOMGUbtuMx1XqHNoR" +
+			// "fXg+oj22QabirZg97RYeh3s7lnBRy1QmFPKu5yF9JSyAspvtuw==");
 
 			if (!trust) {
 				throw new CertificateException("Certificate isn't from mail server!");
