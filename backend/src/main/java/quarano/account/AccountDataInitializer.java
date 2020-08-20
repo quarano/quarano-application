d0ee3e2643378ae8dbc87fcb5f70c76b099a43ad
paul.guhl <paul.guhl@bridging-it.de>
2020-08-20 22:22:17 +0200

    CORE-264: replaced ellipse char by triple dot which stupid simple and works in tests

--- backend/src/main/java/quarano/account/AccountDataInitializer.java
+++ backend/src/main/java/quarano/account/AccountDataInitializer.java
@@ -31,7 +31,7 @@ class AccountDataInitializer implements DataInitializer {
 	@Override
 	public void initialize() {
 
-		log.warn("Test data: creating test accounts…");
+		log.warn("Test data: creating test accounts...");
 
 		// account for GA user
 		accounts.createStaffAccount("admin", UnencryptedPassword.of("admin"), "Mark", "Muster",
