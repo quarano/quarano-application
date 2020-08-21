8e94cb0cd0c10c2f7d3303990f141ac84b5541c9
VILLAN3LL3 <julidarockt@gmail.com>
2020-08-21 11:14:31 +0200

    CORE3-377 Translated initial-questionaire-form.component

--- frontend/libs/client/feature-enrollment/src/lib/initial-questionaire-form/initial-questionaire-form.component.ts
+++ frontend/libs/client/feature-enrollment/src/lib/initial-questionaire-form/initial-questionaire-form.component.ts
@@ -1,3 +1,4 @@
+import { TranslateService } from '@ngx-translate/core';
 import {
   ValidationErrorGenerator,
   VALIDATION_PATTERNS,
@@ -8,6 +9,8 @@ import { Component, Input, OnInit } from '@angular/core';
 import { FormGroup, Validators, ValidatorFn } from '@angular/forms';
 import { SubSink } from 'subsink';
 import { SymptomDto } from '@qro/shared/util-symptom';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'qro-initial-questionaire-form',
@@ -26,20 +29,14 @@ export class InitialQuestionaireFormComponent implements OnInit {
   subs = new SubSink();
   errorGenerator = ValidationErrorGenerator;
 
-  tooltip =
-    'In diesem Fall sind das folgende Vorerkankungen: chronische Herzerkrankung, ' +
-    'Lungenerkrankungen (z. B. Asthma, COPD, chronische Bronchitis), chronische Lebererkrankungen, ' +
-    'Diabetes mellitus (Zuckerkrankheit), Tumor-/Krebserkrankungen, Patienten mit geschwächtem ' +
-    'Immunsystem (inkl. HIV/AIDS)';
+  symptomTooltip$: Observable<string>;
 
-  symptomTooltip: string;
-
-  constructor() {}
+  constructor(private translate: TranslateService) {}
 
   ngOnInit(): void {
-    this.symptomTooltip = `Als Covid-19 charakterische Symptome zählen: ${this.symptoms
-      ?.map((s) => s.name)
-      .join(', ')}`;
+    this.symptomTooltip$ = this.translate
+      .get('INITIAL_QUESTIONAIRE_FORM.CHARAKTERISTISCHE_SYMPTOME')
+      .pipe(map((res) => `${res}: ${this.symptoms?.map((s) => s.name).join(', ')}`));
     this.toggleAdditionalFieldValitations('hasSymptoms', 'dayOfFirstSymptoms', null, [Validators.required]);
     this.toggleAdditionalFieldValitations('hasSymptoms', 'symptoms', [], [ArrayValidator.minLengthArray(1)]);
     this.toggleAdditionalFieldValitations('belongToMedicalStaff', 'belongToMedicalStaffDescription', null, [
