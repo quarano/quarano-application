91b741c790fb7894718d8249a1681151d36f98bc
Jochen Kraushaar <jochen.kraushaar@bridging-it.de>
2020-08-23 11:06:36 +0200

    CORE-409 Mark fields with initial value as touched to trigger validation of fields

--- frontend/libs/client/ui-contact-person-detail/src/lib/contact-person-form/contact-person-form.component.spec.ts
+++ frontend/libs/client/ui-contact-person-detail/src/lib/contact-person-form/contact-person-form.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ContactPersonFormComponent } from './contact-person-form.component';
-import { FormBuilder } from '@angular/forms';
+import {FormBuilder} from '@angular/forms';
 import { SnackbarService } from '@qro/shared/util-snackbar';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ContactPersonService, ContactPersonDto } from '@qro/client/domain';
@@ -30,5 +30,41 @@ describe('ContactPersonFormComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should mark fields with truthy initial value as touched', () => {
+    component.contactPerson = {
+      firstName: 'Test',
+      lastName: 'Meiser1'
+    } as ContactPersonDto;
+    component.ngOnInit();
+
+    expect(component.formGroup.controls.firstName.touched).toBeTruthy();
+    expect(component.formGroup.controls.lastName.touched).toBeTruthy();
+  });
+
+  it('should not mark fields with empty initial value as touched', () => {
+    component.contactPerson = {
+      firstName: 'Test',
+      lastName: 'Meiser1'
+    } as ContactPersonDto;
+    component.ngOnInit();
+
+    Object.keys(component.formGroup.controls).forEach((key: string) => {
+      if (key !== 'firstName' && key !== 'lastName') {
+        expect(component.formGroup.get(key).touched).toBeFalsy();
+      }
+    });
+  });
+
+  it('should validate fields with initial value', () => {
+    component.contactPerson = {
+      firstName: 'Test',
+      lastName: 'Meiser1'
+    } as ContactPersonDto;
+    component.ngOnInit();
+
+    expect(component.formGroup.controls.firstName.valid).toBeTruthy();
+    expect(component.formGroup.controls.lastName.valid).toBeFalsy();
   });
 });
