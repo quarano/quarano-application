ae16e69059bbaea2bd0c06a1dcd44c4ba465dfab
VILLAN3LL3 <julidarockt@gmail.com>
2020-08-24 09:03:54 +0200

    CORE-377 Set datepicker language

--- frontend/libs/shared/util-translation/src/lib/store/language.effects.ts
+++ frontend/libs/shared/util-translation/src/lib/store/language.effects.ts
@@ -1,5 +1,6 @@
+import { DateAdapter } from '@angular/material/core';
 import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { Actions, createEffect, ofType, act } from '@ngrx/effects';
 import { tap } from 'rxjs/operators';
 import { TranslateService } from '@ngx-translate/core';
 import { LanguageActions } from './action-types';
@@ -13,10 +14,11 @@ export class LanguageEffects {
         tap((action) => {
           this.translate.use(action.selectedLanguage.key);
           localStorage.setItem('selectedLanguage', JSON.stringify(action.selectedLanguage));
-        })
+        }),
+        tap((action) => this.dateAdapter.setLocale(action.selectedLanguage.key))
       ),
     { dispatch: false }
   );
 
-  constructor(private actions$: Actions, private translate: TranslateService) {}
+  constructor(private actions$: Actions, private translate: TranslateService, private dateAdapter: DateAdapter<any>) {}
 }
