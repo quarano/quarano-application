1cf6adf3f2e4d87d1aefcd395f720cd267828756
sebastian <clausing.sebastian@gmail.com>
2020-09-08 23:12:51 +0200

    CORE-351: load symptoms only if nothing is cached

--- frontend/libs/shared/util-symptom/src/lib/resolvers/symptoms.resolver.ts
+++ frontend/libs/shared/util-symptom/src/lib/resolvers/symptoms.resolver.ts
@@ -1,14 +1,24 @@
 import { SymptomEntityService } from './../data-access/symptom-entity.service';
-import { Observable } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 import { Injectable } from '@angular/core';
-import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
 import { SymptomDto } from '../model/symptom';
+import { filter, first, switchMap, tap } from 'rxjs/operators';
 
 @Injectable()
-export class SymptomsResolver implements Resolve<SymptomDto[]> {
+export class SymptomsResolver implements Resolve<SymptomDto[] | boolean> {
   constructor(private entityService: SymptomEntityService) {}
 
-  resolve(route: ActivatedRouteSnapshot): Observable<SymptomDto[]> {
-    return this.entityService.getAll();
+  resolve(route: ActivatedRouteSnapshot): Observable<SymptomDto[] | boolean> {
+    return this.entityService.loaded$.pipe(
+      switchMap((loaded) => {
+        if (!loaded) {
+          return this.entityService.getAll();
+        } else {
+          return this.entityService.entities$;
+        }
+      }),
+      first()
+    );
   }
 }
