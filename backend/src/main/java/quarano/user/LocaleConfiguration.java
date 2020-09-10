35855b88c7d98f146675d27df9e82816c2180ffb
Jens Kutzsche <github@gebea.de>
2020-09-10 13:50:46 +0200

    CORE-355 uses locale parameter to change user locale and uses more Spring functions
    - The LocaleChangeInterceptor reacts to the "locale" parameter and
    invokes LocaleResolver.setLocale to set this.
    - The using of this parameter is a result of of a call about multi
    language support. See the comments of the ticket.
    - The determination of the locale from Accept-Language header is now
    done by the superclass of LocaleResolver.

--- backend/src/main/java/quarano/user/LocaleConfiguration.java
+++ backend/src/main/java/quarano/user/LocaleConfiguration.java
@@ -10,7 +10,6 @@ import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Locale;
-import java.util.Locale.LanguageRange;
 import java.util.Optional;
 
 import javax.servlet.FilterChain;
@@ -26,9 +25,11 @@ import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.context.support.MessageSourceAccessor;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
-import org.springframework.util.StringUtils;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 /**
  * Configuration class for locale and message related matters.
@@ -37,7 +38,7 @@ import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
  */
 @Configuration(proxyBeanMethods = false)
 @RequiredArgsConstructor
-public class LocaleConfiguration {
+public class LocaleConfiguration implements WebMvcConfigurer {
 
 	public static final Locale TURKISH = new Locale("tr");
 
@@ -47,7 +48,7 @@ public class LocaleConfiguration {
 			TURKISH);
 
 	private final @NonNull AuthenticationManager accounts;
-	private final @NonNull TrackedPersonRepository persons;
+	private final @NonNull TrackedPersonRepository people;
 
 	@Bean
 	LocaleResolver localeResolver() {
@@ -59,6 +60,16 @@ public class LocaleConfiguration {
 		return new MessageSourceAccessor(source);
 	}
 
+	@Bean
+	LocaleChangeInterceptor localeChangeInterceptor() {
+		return new LocaleChangeInterceptor();
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry interceptorRegistry) {
+		interceptorRegistry.addInterceptor(localeChangeInterceptor());
+	}
+
 	/**
 	 * Resolves the locale from users stored data if set and if not then from requests <code>Accept-Language</code>
 	 * header.
@@ -67,29 +78,33 @@ public class LocaleConfiguration {
 
 		public LocaleResolver() {
 			setDefaultLocale(Locale.GERMANY);
+			setSupportedLocales(LOCALES);
 		}
 
 		@Override
 		public Locale resolveLocale(HttpServletRequest request) {
-			return getLocaleFromTrackedPerson().or(() -> resolveLocaleFrom(request)).orElse(getDefaultLocale());
+			return getLocaleFromTrackedPerson().orElseGet(() -> super.resolveLocale(request));
 		}
 
 		private Optional<Locale> getLocaleFromTrackedPerson() {
 
 			return accounts.getCurrentUser()
-					.flatMap(persons::findByAccount)
+					.flatMap(people::findByAccount)
 					.map(TrackedPerson::getLocale)
 					.filter(it -> !Collections.disjoint(LocaleUtils.localeLookupList(it), LOCALES));
 		}
 
-		private Optional<Locale> resolveLocaleFrom(HttpServletRequest request) {
-
-			var headerLang = request.getHeader(HttpHeaders.ACCEPT_LANGUAGE);
-
-			return Optional.ofNullable(headerLang)
-					.filter(StringUtils::hasText)
-					.map(LanguageRange::parse)
-					.map(it -> Locale.lookup(it, LOCALES));
+		/**
+		 * This method is called by {@link LocaleChangeInterceptor} to set the locale if one was specified by parameter.
+		 * Sets the given {@link Locale} to the {@link TrackedPerson} of the current account and saves this
+		 * {@link TrackedPerson}.
+		 */
+		@Override
+		public void setLocale(HttpServletRequest request, HttpServletResponse response, Locale locale) {
+			accounts.getCurrentUser()
+					.flatMap(people::findByAccount)
+					.map(it -> it.setLocale(locale))
+					.ifPresent(people::save);
 		}
 	}
 
@@ -98,7 +113,7 @@ public class LocaleConfiguration {
 	 */
 	@Component
 	@RequiredArgsConstructor
-	public class SetContentHeaderFilter extends OncePerRequestFilter {
+	class SetContentHeaderFilter extends OncePerRequestFilter {
 
 		private final @NonNull LocaleResolver localeResolver;
 
