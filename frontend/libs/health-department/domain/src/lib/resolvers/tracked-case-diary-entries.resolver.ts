acec8cebb2623eba00cc55f23bc39c5eea70de4a
bit-shube <stephen.hube@bridging-it.de>
2020-09-14 21:02:13 +0200

    CORE-351 resolve formatting issues

--- frontend/libs/health-department/domain/src/lib/resolvers/tracked-case-diary-entries.resolver.ts
+++ frontend/libs/health-department/domain/src/lib/resolvers/tracked-case-diary-entries.resolver.ts
@@ -1,13 +1,12 @@
-import {Observable, of} from 'rxjs';
-import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, Resolve} from '@angular/router';
-import {HealthDepartmentService} from "../data-access/health-department.service";
-import {TrackedCaseDiaryEntryDto} from "@qro/health-department/domain";
+import { Observable, of } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { HealthDepartmentService } from '../data-access/health-department.service';
+import { TrackedCaseDiaryEntryDto } from '@qro/health-department/domain';
 
 @Injectable()
 export class TrackedCaseDiaryEntriesResolver implements Resolve<TrackedCaseDiaryEntryDto[]> {
-  constructor(private healthDepartmentService: HealthDepartmentService) {
-  }
+  constructor(private healthDepartmentService: HealthDepartmentService) {}
 
   resolve(route: ActivatedRouteSnapshot): Observable<TrackedCaseDiaryEntryDto[]> {
     const id = route.paramMap.get('id');
--- frontend/libs/health-department/feature-case-detail/src/lib/diary-entries-list/diary-entries-list.component.ts
+++ frontend/libs/health-department/feature-case-detail/src/lib/diary-entries-list/diary-entries-list.component.ts
@@ -1,8 +1,8 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {TrackedCaseDiaryEntryDto} from "@qro/health-department/domain";
+import { Component, Input, OnInit } from '@angular/core';
+import { TrackedCaseDiaryEntryDto } from '@qro/health-department/domain';
 import * as _ from 'lodash';
-import {TrackedCaseDiaryEntry} from "../diary-entries-list-item/diary-entries-list-item.component";
-import {Dictionary} from "@ngrx/entity";
+import { TrackedCaseDiaryEntry } from '../diary-entries-list-item/diary-entries-list-item.component';
+import { Dictionary } from '@ngrx/entity';
 
 @Component({
   selector: 'qro-diary-entries-list',
@@ -14,11 +14,10 @@ export class DiaryEntriesListComponent implements OnInit {
 
   diaryEntries: TrackedCaseDiaryEntry[] = [];
 
-  private readonly MORNING = "morning";
-  private readonly EVENING = "evening";
+  private readonly MORNING = 'morning';
+  private readonly EVENING = 'evening';
 
-  constructor() {
-  }
+  constructor() {}
 
   ngOnInit() {
     this.mapToEntries(this.entriesDto);
@@ -26,26 +25,28 @@ export class DiaryEntriesListComponent implements OnInit {
 
   private mapToEntries(diaryEntriesDto: TrackedCaseDiaryEntryDto[]) {
     if (!!diaryEntriesDto && diaryEntriesDto.length > 0) {
-
       const groupedDiaryEntryDto = this.groupEntriesByDate(diaryEntriesDto);
 
       const convertedEntries = this.convertToDiaryEntries(groupedDiaryEntryDto);
 
       this.diaryEntries = this.sortByDateDescending(convertedEntries);
     }
-
   }
 
   private convertToDiaryEntries(groupedDiaryEntries: Dictionary<any[]>): TrackedCaseDiaryEntry[] {
-    const mappedEntries: TrackedCaseDiaryEntry[] = []
+    const mappedEntries: TrackedCaseDiaryEntry[] = [];
     for (const [date, diaryEntries] of Object.entries(groupedDiaryEntries)) {
-      const entryMorning = diaryEntries.filter((diaryEntry: TrackedCaseDiaryEntryDto) => diaryEntry.slot.timeOfDay === this.MORNING)?.[0];
-      const entryEvening = diaryEntries.filter((diaryEntry: TrackedCaseDiaryEntryDto) => diaryEntry.slot.timeOfDay === this.EVENING)?.[0];
+      const entryMorning = diaryEntries.filter(
+        (diaryEntry: TrackedCaseDiaryEntryDto) => diaryEntry.slot.timeOfDay === this.MORNING
+      )?.[0];
+      const entryEvening = diaryEntries.filter(
+        (diaryEntry: TrackedCaseDiaryEntryDto) => diaryEntry.slot.timeOfDay === this.EVENING
+      )?.[0];
 
       const entry = {
         date: date,
         morning: entryMorning,
-        evening: entryEvening
+        evening: entryEvening,
       } as TrackedCaseDiaryEntry;
 
       mappedEntries.push(entry);
