ab1e54d4cb142bb5ea86d389ef23be6db2719e0f
Patrick Otto <patrick-otto23@web.de>
2020-10-01 21:50:16 +0200

    Remove environment files of former hosting configurations. Only OTC is relevant anymore.

--- frontend/apps/quarano-frontend/src/environments/environment.prod-evoila.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  api: {
-    baseUrl: 'https://mannheim-api.quarano.de',
-  },
-};
--- frontend/apps/quarano-frontend/src/environments/environment.sec-evoila.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: true,
-  api: {
-    baseUrl: 'https://quarano-backend-sec.cf.evoila.io',
-  },
-};
