76901ad2760d1d31778369470ea18b04c1700161
VILLAN3LL3 <development@gutfrau.de>
2020-10-17 09:06:26 +0200

    CORE-418 Load symptoms after language change

--- frontend/libs/shared/util-translation/src/index.ts
+++ frontend/libs/shared/util-translation/src/index.ts
@@ -4,3 +4,4 @@ export * from './lib/model/language-config';
 export * from './lib/store/action-types';
 export * from './lib/store/selector-types';
 export * from './lib/translate-testing.module';
+export * from './lib/store/language.selectors';
--- frontend/libs/shared/util-translation/src/lib/store/language.effects.ts
+++ frontend/libs/shared/util-translation/src/lib/store/language.effects.ts
@@ -1,3 +1,4 @@
+import { SymptomEntityService } from '@qro/shared/util-symptom';
 import { DateAdapter } from '@angular/material/core';
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType, act } from '@ngrx/effects';
@@ -11,14 +12,18 @@ export class LanguageEffects {
     () =>
       this.actions$.pipe(
         ofType(LanguageActions.languageSelected, LanguageActions.contentLanguageHeaderRead),
-        tap((action) => {
-          this.translate.use(action.selectedLanguage.key);
-          localStorage.setItem('selectedLanguage', JSON.stringify(action.selectedLanguage));
-        }),
-        tap((action) => this.dateAdapter.setLocale(action.selectedLanguage.key))
+        tap((action) => this.translate.use(action.selectedLanguage.key)),
+        tap((action) => localStorage.setItem('selectedLanguage', JSON.stringify(action.selectedLanguage))),
+        tap((action) => this.dateAdapter.setLocale(action.selectedLanguage.key)),
+        tap((action) => this.symptomEntityService.load())
       ),
     { dispatch: false }
   );
 
-  constructor(private actions$: Actions, private translate: TranslateService, private dateAdapter: DateAdapter<any>) {}
+  constructor(
+    private actions$: Actions,
+    private translate: TranslateService,
+    private dateAdapter: DateAdapter<any>,
+    private symptomEntityService: SymptomEntityService
+  ) {}
 }
