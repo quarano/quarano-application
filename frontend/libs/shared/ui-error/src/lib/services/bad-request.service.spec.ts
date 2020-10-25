89d6ca7cc2f14083f536f3160efa397930a9d11b
VILLAN3LL3 <development@gutfrau.de>
2020-10-25 08:29:12 +0100

    CORE-437 fixed unit test

--- frontend/libs/shared/ui-error/src/lib/services/bad-request.service.spec.ts
+++ frontend/libs/shared/ui-error/src/lib/services/bad-request.service.spec.ts
@@ -1,11 +1,15 @@
-import { TranslatedSnackbarService } from '@qro/shared/util-snackbar';
+import { SnackbarService, TranslatedSnackbarService } from '@qro/shared/util-snackbar';
 import { TestBed, inject } from '@angular/core/testing';
 import { BadRequestService } from './bad-request.service';
 
 describe('Service: BadRequest', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [BadRequestService, { provide: TranslatedSnackbarService, useValue: {} }],
+      providers: [
+        BadRequestService,
+        { provide: TranslatedSnackbarService, useValue: {} },
+        { provide: SnackbarService, useValue: {} },
+      ],
     });
   });
 
