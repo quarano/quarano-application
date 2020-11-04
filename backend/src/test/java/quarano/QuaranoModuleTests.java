4cb2a4abb19e3f0b2ff5b75b5c278075a7d16262
Oliver Drotbohm <odrotbohm@icloud.com>
2020-11-04 20:22:08 +0100

    Upgrade to Moduliths 1.1 M1.
    Make use of newly introduce support for custom component groupings to also group Representations and DataInitializers.

--- backend/src/test/java/quarano/QuaranoModuleTests.java
+++ backend/src/test/java/quarano/QuaranoModuleTests.java
@@ -1,12 +1,16 @@
 package quarano;
 
+import static org.moduliths.docs.Documenter.CanvasOptions.Grouping.*;
+
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.Logger;
+import quarano.core.DataInitializer;
 
 import java.io.IOException;
 
 import org.junit.jupiter.api.Test;
 import org.moduliths.docs.Documenter;
+import org.moduliths.docs.Documenter.CanvasOptions;
 import org.moduliths.docs.Documenter.Options;
 import org.moduliths.model.Modules;
 import org.slf4j.LoggerFactory;
@@ -25,11 +29,20 @@ class QuaranoModuleTests {
 		Modules modules = Modules.of(Quarano.class);
 		modules.verify();
 
-		Documenter documenter = new Documenter(modules);
-
 		var options = Options.defaults().withExclusions(it -> it.getName().equals("core"));
 
-		documenter.writeModuleCanvases(null);
+		var dataInitializers = of("Data initializers",
+				"Components to initialize dummy data on application startup.",
+				subtypeOf(DataInitializer.class));
+
+		var representations = of("Representations",
+				"Components to map from domain types to DTOs and vice versa.",
+				nameMatching(".*Representations"));
+
+		var documenter = new Documenter(modules)
+				.writeModuleCanvases(CanvasOptions.defaults()
+						.groupingBy(representations, dataInitializers));
+
 		documenter.writeModulesAsPlantUml(options);
 		modules.forEach(it -> documenter.writeModuleAsPlantUml(it, options));
 	}
