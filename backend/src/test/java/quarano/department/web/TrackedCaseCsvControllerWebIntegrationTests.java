d31102c3785efe9b1f5b7947a4546c168691b4ae
Oliver Drotbohm <odrotbohm@icloud.com>
2020-11-06 12:45:39 +0100

    CORE-487 - Polish web integration test for CSV export.
    Switch to use @TestFactory instead of @ParameterizedTest.

--- backend/src/test/java/quarano/department/web/TrackedCaseCsvControllerWebIntegrationTests.java
+++ backend/src/test/java/quarano/department/web/TrackedCaseCsvControllerWebIntegrationTests.java
@@ -2,16 +2,17 @@ package quarano.department.web;
 
 import static java.time.format.DateTimeFormatter.*;
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.params.provider.Arguments.*;
+import static org.hamcrest.CoreMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 import quarano.AbstractDocumentation;
 import quarano.DocumentationFlow;
 import quarano.QuaranoWebIntegrationTest;
 import quarano.WithQuaranoUser;
 
-import java.io.UnsupportedEncodingException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
@@ -19,68 +20,79 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.DynamicTest;
+import org.junit.jupiter.api.TestFactory;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.lang.Nullable;
 import org.springframework.test.web.servlet.ResultHandler;
 
 /**
  * @author Jens Kutzsche
+ * @author Oliver Drotbohm
  */
 @QuaranoWebIntegrationTest
 @WithQuaranoUser("admin")
 class TrackedCaseCsvControllerWebIntegrationTests extends AbstractDocumentation {
 
-	private String header;
+	private static final String CSV_HEADER = Arrays
+			.stream(TrackedCaseCsvRepresentations.CSV_ORDER)
+			.collect(Collectors.joining(";"));
 
-	@BeforeAll
-	void beforeAll() {
-		header = Arrays.stream(TrackedCaseCsvRepresentations.CSV_ORDER).collect(Collectors.joining(";"));
+	@TestFactory
+	Stream<DynamicTest> getQuarantineOrder() throws Exception {
+
+		var now = LocalDate.now();
+
+		var source = Stream.of(
+				new Valid(null, null, null, 15, false),
+				new Valid("index", null, null, 15, false),
+				new Valid("contact", null, null, 0, false),
+				new Valid("wrongType", null, null, 15, false),
+				new Valid("index", now, null, 15, false),
+				new Valid("index", now.plusDays(1), null, 0, false),
+				new Valid("index", null, now, 15, false),
+				new Valid("index", null, now.minusDays(1), 0, false),
+				new Valid("index", now, now, 15, false),
+				new Valid("index", now.minusDays(1), now.plusDays(1), 15, true),
+				new Valid("index", now.plusDays(1), now.minusDays(1), 0, false));
+
+		return DynamicTest.stream(source.iterator(), Object::toString, bar -> {
+
+			var result = mvc.perform(get("/hd/quarantines")
+					.contentType(MediaType.valueOf("text/csv"))
+					.param("type", bar.getType())
+					.param("from", bar.getFrom())
+					.param("to", bar.getTo()))
+					.andDo(documentGetQuarantineOrder(bar.isDocument()))
+					.andExpect(status().is2xxSuccessful())
+					.andExpect(header().string(HttpHeaders.CONTENT_TYPE, is("text/csv")))
+					.andReturn().getResponse().getContentAsString();
+
+			assertThat(result.lines()).size().isEqualTo(bar.getResultSize());
+
+			if (bar.getResultSize() != 0) {
+				assertThat(result.lines()).first().isEqualTo(CSV_HEADER);
+			}
+		});
 	}
 
-	@ParameterizedTest(name = "{index} get quarantine order for type {0}; from {1}; to {2}")
-	@MethodSource("quarantineParams")
-	void getQuarantineOrder(String type, String from, String to, int resultSize, boolean createDoku) throws Exception {
+	@TestFactory
+	Stream<DynamicTest> getQuarantineOrderWithErrors() throws Exception {
 
-		var result = performGetAndExpect200(type, from, to, createDoku);
+		var sources = Stream.of(
+				new Invalid(null, LocalDate.now(), null),
+				new Invalid(null, null, LocalDate.now()));
 
-		assertThat(result.lines()).size().isEqualTo(resultSize);
+		return DynamicTest.stream(sources.iterator(), Object::toString, foo -> {
 
-		if (resultSize != 0) {
-			assertThat(result.lines()).first().isEqualTo(header);
-		}
-	}
-
-	@ParameterizedTest(name = "{index} get quarantine order for type {0}; from {1}; to {2}")
-	@MethodSource("wrongParams")
-	void getQuarantineOrderWithErrors(String type, String from, String to) throws Exception {
-		performGetAndExpect400(type, from, to);
-	}
-
-	private String performGetAndExpect200(String type, String from, String to, boolean createDoku)
-			throws UnsupportedEncodingException, Exception {
-
-		return mvc.perform(get("/hd/quarantines")
-				.contentType(MediaType.valueOf("text/csv"))
-				.param("type", type)
-				.param("from", from)
-				.param("to", to))
-				.andDo(documentGetQuarantineOrder(createDoku))
-				.andExpect(status().is2xxSuccessful())
-				.andReturn().getResponse().getContentAsString();
-	}
-
-	private void performGetAndExpect400(String type, String from, String to)
-			throws UnsupportedEncodingException, Exception {
-
-		mvc.perform(get("/hd/quarantines")
-				.contentType(MediaType.valueOf("text/csv"))
-				.param("type", type)
-				.param("from", from)
-				.param("to", to))
-				.andExpect(status().is4xxClientError());
+			mvc.perform(get("/hd/quarantines")
+					.contentType(MediaType.valueOf("text/csv"))
+					.param("type", foo.getType())
+					.param("from", foo.getFrom())
+					.param("to", foo.getTo()))
+					.andExpect(status().is4xxClientError());
+		});
 	}
 
 	private static ResultHandler documentGetQuarantineOrder(boolean createDocs) {
@@ -90,28 +102,61 @@ class TrackedCaseCsvControllerWebIntegrationTests extends AbstractDocumentation 
 				: result -> {};
 	}
 
-	private static Stream<Arguments> quarantineParams() {
+	@Getter
+	@AllArgsConstructor
+	private static class Invalid {
 
-		return Stream.of(
-				arguments(null, null, null, 15, false),
-				arguments("index", null, null, 15, false),
-				arguments("contact", null, null, 0, false),
-				arguments("wrongType", null, null, 15, false),
-				arguments("index", LocalDate.now().format(ISO_DATE), null, 15, false),
-				arguments("index", LocalDate.now().plusDays(1).format(ISO_DATE), null, 0, false),
-				arguments("index", null, LocalDate.now().format(ISO_DATE), 15, false),
-				arguments("index", null, LocalDate.now().minusDays(1).format(ISO_DATE), 0, false),
-				arguments("index", LocalDate.now().format(ISO_DATE), LocalDate.now().format(ISO_DATE), 15, false),
-				arguments("index", LocalDate.now().minusDays(1).format(ISO_DATE), LocalDate.now().plusDays(1).format(ISO_DATE),
-						15, true),
-				arguments("index", LocalDate.now().plusDays(1).format(ISO_DATE), LocalDate.now().minusDays(1).format(ISO_DATE),
-						0, false));
+		@Nullable String type;
+		@Nullable LocalDate from, to;
+
+		@Nullable
+		String getFrom() {
+			return toNullableString(from);
+		}
+
+		@Nullable
+		String getTo() {
+			return toNullableString(to);
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see java.lang.Object#toString()
+		 */
+		@Override
+		public String toString() {
+			return String.format("Get quarantine order for type %s from %s to %s", type, from, to);
+		}
+
+		@Nullable
+		protected String toNullableString(@Nullable LocalDate date) {
+			return date == null ? null : date.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT));
+		}
 	}
 
-	private static Stream<Arguments> wrongParams() {
+	@Getter
+	private static class Valid extends Invalid {
 
-		return Stream.of(
-				arguments(null, LocalDate.now().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT)), null),
-				arguments(null, null, LocalDate.now().format(DateTimeFormatter.ofLocalizedDate(FormatStyle.SHORT))));
+		int resultSize;
+		boolean document;
+
+		public Valid(@Nullable String type, @Nullable LocalDate from, @Nullable LocalDate to, int resultSize,
+				boolean document) {
+
+			super(type, from, to);
+
+			this.resultSize = resultSize;
+			this.document = document;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * @see quarano.department.web.TrackedCaseCsvControllerWebIntegrationTests.Foo#toNullableString(java.time.LocalDate)
+		 */
+		@Override
+		@Nullable
+		protected String toNullableString(@Nullable LocalDate date) {
+			return date == null ? null : date.format(ISO_DATE);
+		}
 	}
 }
