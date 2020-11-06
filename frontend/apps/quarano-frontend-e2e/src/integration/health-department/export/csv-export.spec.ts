79e5af2deaabc05f76617fc73f38fbc3d6795352
VILLAN3LL3 <development@gutfrau.de>
2020-11-06 20:15:38 +0100

    CORE-552 completed e2e test

--- frontend/apps/quarano-frontend-e2e/src/integration/health-department/export/csv-export.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/health-department/export/csv-export.spec.ts
@@ -1,19 +1,21 @@
 /// <reference types="cypress" />
 
+import * as moment from 'moment';
+
 describe('csv export', () => {
   beforeEach(() => {
     cy.server();
-    cy.route('GET', '/hd/quarantines/*').as('getcsv');
+    const dateString = moment().format('YYYY-MM-DD');
+    cy.route('GET', `/hd/quarantines?from=${dateString}&to=${dateString}`).as('getcsv');
 
     cy.loginAgent();
+    cy.visit('health-department/export');
   });
 
   describe('csv export happy path', () => {
     it('should get csv data', () => {
-      cy.get('[data-cy="export-submit"]').should('exist').should('be.enabled').click();
+      cy.get('[data-cy="export-submit"] button').should('exist').should('be.enabled').click();
       cy.wait('@getcsv').its('status').should('eq', 200);
-
-      cy.get('[data-cy="case-data-table"]').find('datatable-row-wrapper').should('have.length.greaterThan', 0);
     });
   });
 });
