3d85f017e8bb3ec3163ac3f4c99b18ebd5d54eb4
VILLAN3LL3 <development@gutfrau.de>
2020-11-06 20:47:42 +0100

    CORE-552 Fixed unit tests

--- frontend/libs/health-department/feature-export/src/lib/export/export.component.spec.ts
+++ frontend/libs/health-department/feature-export/src/lib/export/export.component.spec.ts
@@ -1,9 +1,14 @@
-/* tslint:disable:no-unused-variable */
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { SharedUiMaterialModule } from '@qro/shared/ui-material';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SnackbarService } from '@qro/shared/util-snackbar';
+import { BadRequestService } from '@qro/shared/ui-error';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { By } from '@angular/platform-browser';
-import { DebugElement } from '@angular/core';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 import { ExportComponent } from './export.component';
+import { ValidationErrorService } from '@qro/shared/util-forms';
+import { HealthDepartmentService } from '@qro/health-department/domain';
 
 describe('ExportComponent', () => {
   let component: ExportComponent;
@@ -12,6 +17,14 @@ describe('ExportComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ExportComponent],
+      schemas: [NO_ERRORS_SCHEMA],
+      imports: [FormsModule, ReactiveFormsModule, SharedUiMaterialModule, NoopAnimationsModule],
+      providers: [
+        { provide: ValidationErrorService, useValue: { getErrorKeys: () => [] } },
+        { provide: HealthDepartmentService, useValue: {} },
+        { provide: BadRequestService, useValue: {} },
+        { provide: SnackbarService, useValue: {} },
+      ],
     }).compileComponents();
   }));
 
--- frontend/libs/health-department/feature-export/src/lib/export/export.component.ts
+++ frontend/libs/health-department/feature-export/src/lib/export/export.component.ts
@@ -86,15 +86,15 @@ export class ExportComponent implements OnInit {
       return;
     }
     this.loading = true;
-    const { from, to, caseType } = this.formGroup.getRawValue();
+    const { from, to, type } = this.formGroup.getRawValue();
     this.healthDepartmentService
-      .getCsvData(caseType, from, to)
+      .getCsvData(type, from, to)
       .pipe(
         map((result: HttpResponse<string>) => new Blob([result.body], { type: result.headers.get('Content-Type') })),
         tap((blob) => {
           fileSaver.saveAs(
             blob,
-            `csvexport_${caseType || 'alle_faelle'}_${(from as Moment).format('DD.MM.YYYY')}-${(to as Moment).format(
+            `csvexport_${type || 'alle_faelle'}_${(from as Moment).format('DD.MM.YYYY')}-${(to as Moment).format(
               'DD.MM.YYYY'
             )}.csv`
           );
