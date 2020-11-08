1c375273a6b42a845ee1c3c13fe9657703233298
VILLAN3LL3 <development@gutfrau.de>
2020-11-08 19:26:51 +0100

    updated nx

--- frontend/libs/administration/feature-account-detail/jest.config.js
+++ frontend/libs/administration/feature-account-detail/jest.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  name: 'administration-feature-account-detail',
-  preset: '../../../jest.config.js',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/administration/feature-account-detail',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  displayName: 'administration-feature-account-detail',
 };
--- frontend/libs/administration/feature-account-list/jest.config.js
+++ frontend/libs/administration/feature-account-list/jest.config.js
@@ -1,10 +1,21 @@
 module.exports = {
-  name: 'administration-feature-account-list',
-  preset: '../../../jest.config.js',
+  preset: '../../../jest.preset.js',
   coverageDirectory: '../../../coverage/libs/administration/feature-account-list',
   snapshotSerializers: [
     'jest-preset-angular/build/AngularNoNgAttributesSnapshotSerializer.js',
     'jest-preset-angular/build/AngularSnapshotSerializer.js',
     'jest-preset-angular/build/HTMLCommentSerializer.js',
   ],
+  setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
+  globals: {
+    'ts-jest': {
+      tsConfig: '<rootDir>/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.(html|svg)$',
+      astTransformers: [
+        'jest-preset-angular/build/InlineFilesTransformer',
+        'jest-preset-angular/build/StripStylesTransformer',
+      ],
+    },
+  },
+  displayName: 'administration-feature-account-list',
 };
