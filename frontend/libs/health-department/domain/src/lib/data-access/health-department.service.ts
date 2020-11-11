f1e3f58a1cd514afa3568ccc5a144598c55c5d7f
bit-shube <stephen.hube@bridging-it.de>
2020-11-11 19:42:34 +0100

    CORE-351 fix unit test, fix merge errors

--- frontend/libs/health-department/domain/src/lib/data-access/health-department.service.ts
+++ frontend/libs/health-department/domain/src/lib/data-access/health-department.service.ts
@@ -1,12 +1,13 @@
 import { CaseDto } from './../model/case';
 import { API_URL, Link } from '@qro/shared/util-data-access';
 import { Inject, Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpParams, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { CaseActionDto } from '../model/case-action';
 import { distinctUntilChanged, map, shareReplay } from 'rxjs/operators';
 import { AuthStore, CaseType, HealthDepartmentDto } from '@qro/auth/api';
-import { TrackedCaseDiaryEntryDto } from '../..';
+import * as moment from 'moment';
+import { Moment } from 'moment';
 
 @Injectable({
   providedIn: 'root',
@@ -19,25 +20,46 @@ export class HealthDepartmentService {
   }
 
   createCase(caseDetail: CaseDto, type: CaseType): Observable<CaseDto> {
-    return this.httpClient.post<CaseDto>(`${this.apiUrl}/api/hd/cases?type=${type}`, caseDetail).pipe(shareReplay());
+    return this.httpClient.post<CaseDto>(`${this.apiUrl}/hd/cases?type=${type}`, caseDetail).pipe(shareReplay());
   }
 
   updateCase(caseDetail: CaseDto): Observable<CaseDto> {
-    return this.httpClient
-      .put<CaseDto>(`${this.apiUrl}/api/hd/cases/${caseDetail.caseId}`, caseDetail)
-      .pipe(shareReplay());
+    return this.httpClient.put<CaseDto>(`${this.apiUrl}/hd/cases/${caseDetail.caseId}`, caseDetail).pipe(shareReplay());
   }
 
   addComment(caseId: string, comment: string): Observable<any> {
-    return this.httpClient.post(`${this.apiUrl}/api/hd/cases/${caseId}/comments`, { comment }).pipe(shareReplay());
+    return this.httpClient.post(`${this.apiUrl}/hd/cases/${caseId}/comments`, { comment }).pipe(shareReplay());
   }
 
   getCase(caseId: string): Observable<CaseDto> {
-    return this.httpClient.get<CaseDto>(`${this.apiUrl}/api/hd/cases/${caseId}`).pipe(shareReplay());
+    return this.httpClient.get<CaseDto>(`${this.apiUrl}/hd/cases/${caseId}`).pipe(shareReplay());
   }
 
   getCaseActions(caseId: string): Observable<CaseActionDto> {
-    return this.httpClient.get<CaseActionDto>(`${this.apiUrl}/api/hd/actions/${caseId}`).pipe(shareReplay());
+    return this.httpClient.get<CaseActionDto>(`${this.apiUrl}/hd/actions/${caseId}`).pipe(shareReplay());
+  }
+
+  getCsvData(caseType: CaseType, start: Moment, end: Moment): Observable<any> {
+    const params: { [param: string]: string | string[] } = {};
+
+    if (caseType) {
+      params['type'] = caseType;
+    }
+    if (start && end) {
+      params['from'] = start.format('YYYY-MM-DD');
+      params['to'] = end.format('YYYY-MM-DD');
+    }
+
+    const options: Object = {
+      headers: new HttpHeaders({
+        'Content-Type': 'text/csv',
+      }),
+      responseType: 'text',
+      params,
+      observe: 'response',
+    };
+
+    return this.httpClient.get<string>(`${this.apiUrl}/hd/quarantines`, options).pipe(shareReplay());
   }
 
   public get healthDepartment$(): Observable<HealthDepartmentDto> {
