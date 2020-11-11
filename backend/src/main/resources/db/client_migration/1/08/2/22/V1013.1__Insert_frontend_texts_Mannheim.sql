daf0a43ef701f6df41bbc6b7f7fd0978dee27a3e
Jens Kutzsche <github@gebea.de>
2020-11-11 13:42:08 +0100

    CORE-576 includes the new texts from GA Mannheim

--- backend/src/main/resources/db/client_migration/1/08/2/22/V1013.1__Insert_frontend_texts_Mannheim.sql
+++ backend/src/main/resources/db/client_migration/1/08/2/22/V1013.1__Insert_frontend_texts_Mannheim.sql
@@ -2,9 +2,7 @@ DELETE FROM public.frontend_texts WHERE frontend_text_id = '959b477e-2b29-4e2d-9
 INSERT INTO public.frontend_texts VALUES ('959b477e-2b29-4e2d-9a4f-f53f590c5adc', 'welcome-index', 'de',
 '<section>
   <p>
-    Sie haben vom Gesundheitsamt Mannheim einen personalisierten Registrierungs-Link erhalten. In den folgenden
-    Seiten melden Sie sich bitte an und nutzen die Anwendung. Mit der Nutzung dieser Anwendung unterstützen Sie das
-    Gesundheitsamt und tragen dazu bei, dass schneller agiert werden kann.
+    Sie haben vom Gesundheitsamt Mannheim einen personalisierten Registrierungs-Link erhalten. In den folgenden Seiten melden Sie sich bitte an und nutzen die Anwendung. Mit der Nutzung dieser Anwendung unterstützen Sie das Gesundheitsamt und tragen dazu bei, dass schneller agiert werden kann.
   </p>
   <p>
     Mit der Anwendung können Sie:
@@ -16,22 +14,17 @@ INSERT INTO public.frontend_texts VALUES ('959b477e-2b29-4e2d-9a4f-f53f590c5adc'
   </ul>
   <h3><strong>Infos zur Registrierung</strong></h3>
   <p>
-    Registrieren Sie sich mit einem Benutzernamen und einem frei wählbaren Passwort. Danach melden Sie sich mit
-    diesen Nutzerdaten an und können quarano nutzen:
+    Registrieren Sie sich mit einem Benutzernamen und einem frei wählbaren Passwort. Danach melden Sie sich mit diesen Nutzerdaten an und können quarano nutzen:
   </p>
   <h3><strong>Infos zur Nutzung</strong></h3>
   <p>
-    Ihre Stammdaten sind nur einmal einzugeben, Kontaktpersonen und mögliche Krankheitszeichen sind zweimal täglich
-    einzugeben.
+    Ihre Stammdaten sind nur einmal einzugeben, Kontaktpersonen und mögliche Krankheitszeichen sind zweimal täglich einzugeben.
   </p>
   <p>
-    <strong>Wichtig:</strong> Dieser Link ist nur für Sie persönlich gültig. Wenn mehrere betroffene Personen im
-    selben Haushalt leben, ist es wichtig, dass sich jede Person mit einem eigenen Zugangslink vom Gesundheitsamt
-    jeweils einen eigenen Benutzer anlegt. So können alle Einträge immer richtig zugeordnet werden.
+    <strong>Wichtig:</strong> Dieser Link ist nur für Sie persönlich gültig. Wenn mehrere betroffene Personen im selben Haushalt leben, ist es wichtig, dass sich jede Person mit einem eigenen Zugangslink vom Gesundheitsamt jeweils einen eigenen Benutzer anlegt. So können alle Einträge immer richtig zugeordnet werden.
   </p>
   <p>
-    Bei Fragen scheuen Sie sich bitte nicht, uns zu kontaktieren. Bei allgemeinen Fragen zum Thema Corona wenden Sie
-    sich bitte an die Corona-Hotline der Stadt Mannheim unter der Telefonnummer
+    Bei allgemeinen Fragen zum Thema Corona wenden Sie sich bitte an die Corona-Hotline der Stadt Mannheim unter der Telefonnummer
     <a href="tel:+496212932253" class="phone-link">0621-293 2253</a>:
   </p>
   <ul>
@@ -39,15 +32,10 @@ INSERT INTO public.frontend_texts VALUES ('959b477e-2b29-4e2d-9a4f-f53f590c5adc'
     <li>Samstags sowie an Sonn- und Feiertagen von 9:00 – 14:00 Uhr</li>
   </ul>
   <p>Ihr Gesundheitsamt Mannheim</p>
-  <p class="footnote">
-    <em
-      >Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin. Nachts und
-      am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die
-      Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen zögern Sie bitte nicht, den
-      Rettungsdienst unter der 112 zu rufen. Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss
-      die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne
-      stehen.</em
-    >
+  <p class="footnote">"
+    <em>
+          Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin. Nachts und am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen rufen Sie den Notruf: 112. Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne stehen.
+    </em>
   </p>
 </section>', NULL, NULL, NULL, NULL);
 
@@ -55,40 +43,40 @@ DELETE FROM public.frontend_texts WHERE frontend_text_id = '1acc1dd5-8319-402d-b
 INSERT INTO public.frontend_texts VALUES ('1acc1dd5-8319-402d-bb38-50db22f8a429', 'welcome-index', 'en',
 '<section>
   <p>
-    You have received a personalized registration link from the Gesundheitsamt Mannheim. On the following pages you can log in and use the application. By using the application you can support the Gesundheitsamt and do your part to ensure that fast action can be taken.
+    You have received a personalized registration link from the health agency Mannheim. On the following pages please register and use the application. By using this application you help the health agency to react faster.
   </p>
   <p>
-    With the application you can:
+    Within the aplication you can:
   </p>
   <ul>
-    <li>Update your basic information</li>
-    <li>Enter contacts</li>
-    <li>Record any signs of illness</li>
+    <li>Update your master data</li>
+    <li>Record contact persons</li>
+    <li>Record symptoms that may indicate a infection</li>
   </ul>
-  <h3><strong>Infos for registration</strong></h3>
+  <h3><strong>Info for registration:</strong></h3>
   <p>
-    Register with a user name and your choice of password. You can then use this information to log in and use quarano
+    Register with a user name and a freely definable password. Afterwards you can start using quarano:
   </p>
-  <h3><strong>Infos for use</strong></h3>
+  <h3><strong>Info for usage:</strong></h3>
   <p>
-    You only need to enter your basic personal information once. Contacts and possible symptoms should be recorded twice daily.
-  </p>
-  <p>
-    <strong>Important:</strong> This link is for your personal use only. If several affected people are living together in one household, it is important that each person registers as a separate user with their own personalized access link from the health authority. In this way, all the information entered can be correlated accurately.
+    Your master data only needs to be maintained once. Contacts and symptoms are to be maintained twice a day.
   </p>
   <p>
-    If you have any questions, please do not hesitate to contact us. If you have general questions about Corona, please call Mannheim''s Corona-Hotline under the phone number
+    <strong>Important:</strong> This link is only valid for you personally In case multiple affected people live in the same household, it is important that each houshold member creates an own user account based on an own registration link issued by the health agency. This allows that all data can be mapped to the correct person at all times.
+  </p>
+  <p>
+    For general questions in regards to Corona feel free to call the hotline of the city of Mannheim under
     <a href="tel:+496212932253" class="phone-link">0621-293 2253</a>:
   </p>
   <ul>
-    <li>Monday to Friday 9:00 – 17:00</li>
-    <li>Saturday, Sunday and public holidays 9:00 – 14:00</li>
+    <li>Monday through Friday from 9:00 – 17:00 Uhr</li>
+    <li>Saturdays, Sundays and on public holidays from 9:00 – 14:00 Uhr</li>
   </ul>
-  <p>Your Gesundheitsamt Mannheim</p>
+  <p>Your health agency Mannheim</p>
   <p class="footnote">
-    <em
-      >If you need urgent medical treatment, please contact your general practitioner (GP). During the night and at weekends please contact the GP on-call service (116 117) or in severe cases the emergency room at the clinics. In case of acute emergency do not hesitate to call the ambulance service under the number 112. Before going to see your GP or going to the hospital in person, you must first call and tell them you have COVID-19 and are currently in quarantine.</em
-    >
+    <em>
+          In case you require urgent medical treatment please contact your regular doctor - at night or during weekends the emergency on call service (116 117) in german language can be contacted or in serious cases the emgency of the hospitals. In critical situations please do not hesitate calling 112. Please note that in case of personal appearance at your doctor or in a hospital you shall infrom the respective beforehand informing about your relation to COVID-19 and an ongoing quarantine.
+    </em>
   </p>
 </section>', NULL, NULL, NULL, NULL);
 
@@ -142,14 +130,14 @@ INSERT INTO public.frontend_texts VALUES ('a6d20591-660c-48e0-8620-4151164272f3'
   </ul>
   <p>Ihr Gesundheitsamt Mannheim</p>
   <p class="footnote">
-    <em
-      >Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin. Nachts und
+    <em>
+      Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin. Nachts und
       am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die
       Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen zögern Sie bitte nicht, den
       Rettungsdienst unter der 112 zu rufen. Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss
       die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne
-      stehen.</em
-    >
+      stehen.
+    </em>
   </p>
 </section>', NULL, NULL, NULL, NULL);
 
@@ -195,8 +183,8 @@ INSERT INTO public.frontend_texts VALUES ('de909807-45fd-4160-8bff-c42759c775d7'
   </ul>
   <p>Your Gesundheitsamt Mannheim</p>
   <p class="footnote">
-    <em
-      >If you need urgent medical treatment, please contact your general practitioner (GP). During the night and at weekends please contact the GP on-call service (116 117) or in severe cases the emergency room at the clinics. In case of acute emergency do not hesitate to call the ambulance service under the number 112. Before going to see your GP or going to the hospital in person, you must first call and tell them you have COVID-19 and are currently in quarantine.</em
-    >
+    <em>
+      If you need urgent medical treatment, please contact your general practitioner (GP). During the night and at weekends please contact the GP on-call service (116 117) or in severe cases the emergency room at the clinics. In case of acute emergency do not hesitate to call the ambulance service under the number 112. Before going to see your GP or going to the hospital in person, you must first call and tell them you have COVID-19 and are currently in quarantine.
+    </em>
   </p>
 </section>', NULL, NULL, NULL, NULL);
