5bd454e0df3664ef40d1710f6a5aaa489b57a9ed
VILLAN3LL3 <development@gutfrau.de>
2020-11-12 16:49:22 +0100

    update to ng 11

--- frontend/apps/quarano-frontend/src/app/app-routing.module.ts
+++ frontend/apps/quarano-frontend/src/app/app-routing.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload' })],
+  imports: [RouterModule.forRoot(routes, { onSameUrlNavigation: 'reload', relativeLinkResolution: 'legacy' })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
