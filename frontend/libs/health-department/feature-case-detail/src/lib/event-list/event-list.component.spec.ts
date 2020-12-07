394417b17fc78100004c962c3a418d6bfe0d6277
sebastian <clausing.sebastian@gmail.com>
2020-12-07 20:25:50 +0100

    #US614: added new event-list-component with routing

---
+++ frontend/libs/health-department/feature-case-detail/src/lib/event-list/event-list.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { EventListComponent } from './event-list.component';
+
+describe('EventListComponent', () => {
+  let component: EventListComponent;
+  let fixture: ComponentFixture<EventListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [EventListComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(EventListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ frontend/libs/health-department/feature-case-detail/src/lib/event-list/event-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'qro-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.scss'],
+})
+export class EventListComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
