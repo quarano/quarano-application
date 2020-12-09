0b553700d00fa7376ae5b2863ed9d9318b794e47
VILLAN3LL3 <development@gutfrau.de>
2020-12-09 19:08:37 +0100

    CORE-648 fixed unit test

--- frontend/apps/quarano-frontend/src/app/layout/footer/footer.component.spec.ts
+++ frontend/apps/quarano-frontend/src/app/layout/footer/footer.component.spec.ts
@@ -1,20 +1,25 @@
 import { TranslateTestingModule } from '@qro/shared/util-translation';
-/* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { FooterComponent } from './footer.component';
 import { RouterTestingModule } from '@angular/router/testing';
+import { VersionService } from '@qro/general/api';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatMenuModule } from '@angular/material/menu';
 
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      imports: [RouterTestingModule, TranslateTestingModule],
-      declarations: [FooterComponent],
-    }).compileComponents();
-  }));
+  beforeEach(
+    waitForAsync(() => {
+      TestBed.configureTestingModule({
+        imports: [MatMenuModule, RouterTestingModule, TranslateTestingModule],
+        declarations: [FooterComponent],
+        providers: [{ provide: VersionService, useValue: {} }],
+        schemas: [NO_ERRORS_SCHEMA],
+      }).compileComponents();
+    })
+  );
 
   beforeEach(() => {
     fixture = TestBed.createComponent(FooterComponent);
