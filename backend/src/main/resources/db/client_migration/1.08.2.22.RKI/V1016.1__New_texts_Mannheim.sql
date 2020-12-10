5bf5d90a3c8c9d358cd575ac662d83daf791fd1c
Jens Kutzsche <github@gebea.de>
2020-12-10 06:53:54 +0100

    CORE-629 Updates the specific texts of Mannheim

---
+++ backend/src/main/resources/db/client_migration/1.08.2.22.RKI/V1016.1__New_texts_Mannheim.sql
@@ -0,0 +1,296 @@
+-- welcome-index de
+UPDATE public.frontend_texts  SET text =
+'<section>
+  <p>
+    Sie haben vom Gesundheitsamt Mannheim einen personalisierten Registrierungs-Link erhalten. In den folgenden Seiten melden Sie sich bitte an und nutzen die Anwendung. Mit der Nutzung dieser Anwendung unterstützen Sie das Gesundheitsamt und tragen dazu bei, dass schneller agiert werden kann.
+  </p>
+  <p>
+    Mit der Anwendung können Sie:
+  </p>
+  <ul>
+    <li>Ihre Stammdaten aktualisieren</li>
+    <li>Kontaktpersonen angeben</li>
+    <li>Mögliche Krankheitszeichen protokollieren</li>
+  </ul>
+  <h3><strong>Infos zur Registrierung:</strong></h3>
+  <p>
+    Registrieren Sie sich mit einem frei wählbaren Benutzernamen und einem frei wählbaren Passwort. Danach melden Sie sich mit diesen Nutzerdaten an und können quarano nutzen:
+  </p>
+  <h3><strong>Infos zur Nutzung:</strong></h3>
+  <p>
+    Ihre Stammdaten sind nur einmal einzugeben, Kontaktpersonen und mögliche Krankheitszeichen sind zweimal täglich einzugeben.
+  </p>
+  <p>
+    <strong>Wichtig:</strong> Dieser Link ist nur für Sie persönlich gültig. Wenn mehrere betroffene Personen im selben Haushalt leben, ist es wichtig, dass sich jede Person mit einem eigenen Zugangslink vom Gesundheitsamt jeweils einen eigenen Benutzer anlegt. So können alle Einträge immer richtig zugeordnet werden.
+  </p>
+  <p>
+    Bei allgemeinen Fragen zum Thema Corona wenden Sie sich bitte an die Corona-Hotline der Stadt Mannheim unter der Telefonnummer <a href="tel:+496212932253" class="phone-link">0621-293 2253</a>:
+  </p>
+  <ul>
+    <li>Montag bis Freitag von 9:00 – 17:00 Uhr</li>
+    <li>Samstags sowie an Sonn- und Feiertagen von 9:00 – 14:00 Uhr</li>
+  </ul>
+  <p>Ihr Gesundheitsamt Mannheim</p>
+  <p class="footnote">
+    <em>
+        Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin.
+        Nachts und am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen rufen Sie den Notruf: 112.
+        Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne stehen.
+    </em>
+  </p>
+</section>' WHERE frontend_text_id = '959b477e-2b29-4e2d-9a4f-f53f590c5adc';
+
+-- welcome-index en
+UPDATE public.frontend_texts  SET text =
+'<section>
+  <p>
+    You have received a personalized registration link from the health agency Mannheim. On the following pages please register and use the application. By using this application you help the health agency to react faster.
+  </p>
+  <p>
+    Within the aplication you can:
+  </p>
+  <ul>
+    <li>Update your master data</li>
+    <li>Record contact persons</li>
+    <li>Record symptoms that may indicate a infection</li>
+  </ul>
+  <h3><strong>Info for registration:</strong></h3>
+  <p>
+    Register with a user name of your choice and a freely definable password. Afterwards you can start using quarano:
+  </p>
+  <h3><strong>Info for usage:</strong></h3>
+  <p>
+    Your master data only needs to be maintained once. Contacts and symptoms are to be maintained twice a day.
+  </p>
+  <p>
+    <strong>Important:</strong> This link is only valid for you personally In case multiple affected people live in the same household, it is important that each houshold member creates an own user account based on an own registration link issued by the health agency. This allows that all data can be mapped to the correct person at all times.
+  </p>
+  <p>
+    For general questions in regards to Corona feel free to call the hotline of the city of Mannheim under <a href="tel:+496212932253" class="phone-link">0621-293 2253</a>.
+  </p>
+  <ul>
+    <li>Monday through Friday from 9:00 – 17:00 Uhr</li>
+    <li>Saturdays, Sundays and on public holidays from 9:00 – 14:00 Uhr</li>
+  </ul>
+  <p>Your health agency Mannheim</p>
+  <p class="footnote">
+    <em>
+        In case you require urgent medical treatment please contact your regular doctor.
+        At night or during weekends the emergency on call service (116 117) in german language can be contacted or in serious cases the emergency of the hospitals. In critical situations please do not hesitate calling 112.
+        Please note that in case of personal appearance at your doctor or in a hospital you shall infrom the respective beforehand informing about your relation to COVID-19 and an ongoing quarantine.
+    </em>
+  </p>
+</section>' WHERE frontend_text_id = '1acc1dd5-8319-402d-bb38-50db22f8a429';
+
+-- welcome-contact de
+UPDATE public.frontend_texts  SET text =
+'<section>
+  <p>
+    Sie haben vom Gesundheitsamt Mannheim einen personalisierten Registrierungs-Link erhalten. In den folgenden Seiten melden Sie sich bitte an und nutzen die Anwendung. Mit der Nutzung dieser Anwendung unterstützen Sie das Gesundheitsamt und tragen dazu bei, dass schneller agiert werden kann.
+  </p>
+  <p>
+    Mit der Anwendung können Sie:
+  </p>
+  <ul>
+    <li>Ihre Stammdaten aktualisieren</li>
+    <li>Mögliche Krankheitszeichen protokollieren</li>
+  </ul>
+  <h3><strong>Infos zur Registrierung:</strong></h3>
+  <p>
+    Registrieren Sie sich mit einem frei wählbaren Benutzernamen und einem frei wählbaren Passwort. Danach melden Sie sich mit diesen Nutzerdaten an und können quarano nutzen:
+  </p>
+  <h3><strong>Infos zur Nutzung:</strong></h3>
+  <p>
+    Ihre Stammdaten sind nur einmal einzugeben, mögliche Krankheitszeichen sind zweimal täglich einzugeben.
+  </p>
+  <p>
+    <strong>Wichtig:</strong> Dieser Link ist nur für Sie persönlich gültig. Wenn mehrere betroffene Personen im selben Haushalt leben, ist es wichtig, dass sich jede Person mit einem eigenen Zugangslink vom Gesundheitsamt jeweils einen eigenen Benutzer anlegt. So können alle Einträge immer richtig zugeordnet werden.
+  </p>
+  <p>
+    Bei allgemeinen Fragen zum Thema Corona wenden Sie sich bitte an die Corona-Hotline der Stadt Mannheim unter der Telefonnummer <a href="tel:+496212932253" class="phone-link">0621-293 2253</a>:
+  </p>
+  <ul>
+    <li>Montag bis Freitag von 9:00 – 17:00 Uhr</li>
+    <li>Samstags sowie an Sonn- und Feiertagen von 9:00 – 14:00 Uhr</li>
+  </ul>
+  <p>Ihr Gesundheitsamt Mannheim</p>
+  <p class="footnote">
+    <em>
+        Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin.
+        Nachts und am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen rufen Sie den Notruf: 112.
+        Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne stehen.
+    </em>
+  </p>
+</section>' WHERE frontend_text_id = 'a6d20591-660c-48e0-8620-4151164272f3';
+
+-- welcome-contact en
+UPDATE public.frontend_texts  SET text =
+'<section>
+  <p>
+    You have received a personalized registration link from the health agency Mannheim. On the following pages please register and use the application. By using this application you help the health agency to react faster. 
+  </p>
+  <p>
+    Within the aplication you can:
+  </p>
+  <ul>
+    <li>Update your master data</li>
+    <li>Record symptoms that may indicate a infection</li>
+  </ul>
+  <h3><strong>Info for registration:</strong></h3>
+  <p>
+    Register with a user name of your choice and a freely definable password. Afterwards you can start using quarano: 
+  </p>
+  <h3><strong>Info for usage:</strong></h3>
+  <p>
+    Your master data only needs to be maintained once. Symptoms are to be maintained twice a day. 
+  </p>
+  <p>
+    <strong>Important:</strong> This link is only valid for you personally In case multiple affected people live in the same household, it is important that each houshold member creates an own user account based on an own registration link issued by the health agency. This allows that all data can be mapped to the correct person at all times. 
+  </p>
+  <p>
+    For general questions in regards to Corona feel free to call the hotline of the city of Mannheim under <a href="tel:+496212932253" class="phone-link">0621-293 2253</a>.
+  </p>
+  <ul>
+    <li>Monday through Friday from 9:00 – 17:00 Uhr</li>
+    <li>Saturdays, Sundays and on public holidays from 9:00 – 14:00 Uhr</li>
+  </ul>
+  <p>Your health agency Mannheim</p>
+  <p class="footnote">
+    <em>
+        In case you require urgent medical treatment please contact your regular doctor.
+        At night or during weekends the emergency on call service (116 117) in german language can be contacted or in serious cases the emergency of the hospitals. In critical situations please do not hesitate calling 112.
+        Please note that in case of personal appearance at your doctor or in a hospital you shall infrom the respective beforehand informing about your relation to COVID-19 and an ongoing quarantine.
+    </em>
+  </p>
+</section>' WHERE frontend_text_id = 'de909807-45fd-4160-8bff-c42759c775d7';
+
+-- registration-contact de
+UPDATE public.email_texts  SET text =
+'Sehr geehrte/geehrter Frau/Herr {lastName},
+
+
+Sie wurden vom Gesundheitsamt Mannheim kontaktiert und als enge Kontaktperson zu einem bestätigten Covid-19 Fall eingestuft und es wurde eine Quarantäne bis {quarantineEndDate} angeordnet. 
+
+Wie mit Ihnen am Telefon besprochen, melden Sie sich bitte umgehend in unserer Online-Anwendung zur Erfassung möglicher Symptome an unter:
+
+https://{host}/client/enrollment/landing/contact/{activationCode}
+
+Bitte erfassen Sie dort Ihre Stammdaten. Zudem bitten wir Sie, täglich zweimal Fieber zu messen sowie auftretende Symptome (wie z.B. Atembeschwerden, Glieder- oder Muskelschmerzen, Durchfall) direkt in das Online-Tool mit aufzunehmen.
+
+Bei Fragen scheuen Sie sich bitte nicht, uns zu kontaktieren. Bei allgemeinen Fragen zum Thema Corona wenden Sie sich bitte an die Corona-Hotline der Stadt Mannheim unter der Telefonnummer 0621-293 2253.
+
+•   Montag bis Freitag von 9:00 – 17:00 Uhr
+•   Samstags sowie an Sonn- und Feiertagen von 9:00 – 14:00 Uhr 
+
+Bei Fragen, die Sie als Kontaktperson im Speziellen betreffen, wenden Sie sich bitte an die Hotline für Kontaktpersonen unter der Telefonnummer: 0621 -293 2212.
+
+•   Montag bis Freitag von 8:30-13:30 Uhr
+
+
+Im Anhang erhalten Sie außerdem Informationen und Unterlagen, die für Sie als Kontaktperson wichtig sind. 
+
+
+Wir bedanken uns für Ihre Mitarbeit und wünschen Ihnen alles Gute. 
+
+Ihr Gesundheitsamt Mannheim
+
+
+Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin. Nachts und am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen zögern Sie bitte nicht, den Rettungsdienst unter der 112 zu rufen. Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne stehen.
+' WHERE email_text_id = '5ddc2853-40b8-47f2-8a21-55f4041f5363';
+
+-- registration-contact en
+UPDATE public.email_texts  SET text =
+'Dear Mrs/Mr {lastName},
+
+You have been contacted by the health agency and considered as a person with close contact to a confirmed COVID-19 case and a quarantine has been ordered until including {quarantineEndDate}.
+
+As discussed on the phone, please immediately register in our online application in order to capturing potential symptoms with the following link: 
+
+https://{host}/client/enrollment/landing/contact/{activationCode}
+
+Within the aplication please maintain your master data and name possible contacts you had.
+In addition we ask you to check your body temperature twice a day and capture the results within the online tool together with any occuring symptoms (like for example breathing difficulties, aching limbs or muscles, diarrhea).
+
+In case of questions do not hesitate contacting us.
+For general questions in regards to Corona feel free to call the hotline of the city of Mannheim under 0621-293 2253.
+
+• Monday through Friday from 9:00 – 17:00 Uhr
+• Saturdays, Sundays and on public holidays from 9:00 – 14:00 Uhr 
+
+In case of questions specifically in the context of contact persons please call the specific hotline of contact persons: 0621 -293 2212.
+
+• Monday through Friday from 8:30 – 13:30 Uhr
+
+Attached you find additional information and documents that are important for ou as a contact person.
+
+We''d like to thank you for your cooperation and wish you all the best.
+
+Your Mannheim Health Angency
+
+In case you require urgent medical treatment please contact your regular doctor.
+At night or during weekends the emergency on call service (116 117) in german language can be contacted or in serious cases the emergency of the hospitals. In critical situations please do not hesitate calling 112.
+Please note that in case of personal appearance at your doctor or in a hospital you shall infrom the respective beforehand informing about your relation to COVID-19.
+' WHERE email_text_id = 'cee04c8f-427c-414b-883c-213d99744b4d';
+
+-- registration-index de
+UPDATE public.email_texts  SET text =
+'Sehr geehrte/geehrter Frau/Herr {lastName},
+
+Das Gesundheitsamt Mannheim hat Ihnen soeben mitgeteilt, dass Sie mit dem neuartigen Coronavirus SARS-CoV2 infiziert und somit ansteckungsfähig sind. Die Stadt Mannheim hat zur Regelung der Quarantäne eine Allgemeinverfügung erlassen. Diese ist ab dem 28.10.2020 gültig und kann unter folgendem Link eingesehen werden:
+https://www.mannheim.de/quarantäne
+
+Die Quarantäne ist für mindestens 10 Tage ab Abnahme des ersten positiven Abstrichs einzuhalten.
+Personen, die mit Ihnen im gleichen Haushalt wohnen und die bisher nicht selbst positiv getestet wurden, müssen eine Quarantäne von 14 Tagen ab dem Tag des ersten positiven Abstrichs in Ihrem Haushalt einhalten. Für weitere positiv getestete Personen in Ihrem Haushalt gelten die gleichen Quarantäneregeln wie für Sie, d.h. eine Quarantänedauer von mindestens 10 Tagen ab dem Tag des jeweiligen Abstrichs.
+
+Nichteinhaltung dieser Quarantäneregeln kann dazu führen, dass dies als Ordnungswidrigkeit oder gegebenenfalls als Straftat verfolgt wird.
+Das Gesundheitsamt ist verpflichtet, zu ermitteln, wo Sie sich infiziert haben könnten und wer durch Sie angesteckt worden sein könnte. 
+
+Wie mit Ihnen am Telefon besprochen, melden Sie sich bitte umgehend in unserer Online-Anwendung zur Erfassung der Kontakte und Symptome an unter:
+
+https://{host}/client/enrollment/landing/index/{activationCode}
+
+Bitte erfassen Sie dort Ihre Stammdaten und tragen Sie mögliche Kontaktpersonen ein, so dass wir zu diesen Kontakt aufnehmen können.
+
+
+Bei allgemeinen Fragen zum Thema Corona wenden Sie sich bitte an die Corona-Hotline der Stadt Mannheim unter der Telefonnummer 0621-293 2253:
+
+•   Montag bis Freitag von 9:00 – 17:00 Uhr
+•   Samstags sowie an Sonn- und Feiertagen von 9:00 – 14:00 Uhr 
+
+
+Wenn Sie akut ärztliche Behandlung benötigen, wenden Sie sich an Ihren Hausarzt/Ihre Hausärztin. Nachts und am Wochenende stehen der hausärztliche Bereitschaftsdienst (116 117) oder in schwerwiegenden Fällen die Notaufnahmen der Kliniken als Anlaufstelle zur Verfügung. In akuten Notfällen rufen Sie den Notruf: 112. Vor einer persönlichen Vorstellung beim Arzt oder im Krankenhaus muss die telefonische Ankündigung mit Hinweis auf COVID-19 erfolgen und dass Sie aktuell unter Quarantäne stehen
+
+
+Mit freundlichen Grüßen,
+' WHERE email_text_id = '94d0a37e-daed-465c-8bcb-ffdaca9062a3';
+
+-- registration-index en
+UPDATE public.email_texts  SET text = 
+'Dear Mrs/Mr {lastName},
+
+The health agency Mannheim has just informed you about the fact that you are infected with the new Corona virus SARS-CoV2 and as such you are infectious. The city of Mannheim has issued a general decree that regulates the quarantine. This is valid from 28-Oct-2020 and can be checked under the following link:
+https://www.mannheim.de/quarantäne
+
+The quarantine is to be adhered for at least 10 days starzing with the date of the first positive test sample.
+People, that are living with you within the same household and that have up to now not been tested positive must adhere to a 14 days quaarantine starting with the day of the first positive test within the household. For additionally people within youtr household that are tested positive the same rules apply as for yourself, meaning a quarantine for at least 10 days staring with the day of the respective test.
+
+The non-adherence to the quarantine rules can lead to persecution as adminstrative offense or possibly a criminal offense.
+The health agency is obliged to determine where you could have been infected and which otehr people might have been infected by you.
+
+As discussed on the phone, please immediately register in our online application for capturing of contacts and symptoms with the following link:
+
+https://{host}/client/enrollment/landing/index/{activationCode}
+
+Within the aplication please maintain your master data and name possible contacts you had, so that we can contact them.
+
+For general questions in regards to Corona feel free to call the hotline of the city of Mannheim under 0621-293 2253.
+
+• Monday through Friday from 9:00 – 17:00 Uhr
+• Saturdays, Sundays and on public holidays from 9:00 – 14:00 Uhr 
+
+In case you require urgent medical treatment please contact your regular doctor.
+At night or during weekends the emergency on call service (116 117) in german language can be contacted or in serious cases the emergency of the hospitals. In critical situations please do not hesitate calling 112.
+Please note that in case of personal appearance at your doctor or in a hospital you shall infrom the respective beforehand informing about your relation to COVID-19 and an ongoing quarantine.
+
+Best regards,
+' WHERE email_text_id = '58541fa0-c68a-4011-9b78-0b30a8406064';
