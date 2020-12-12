52b6ae0909ed63deb75001b0405ebd481de8e977
sebastian <clausing.sebastian@gmail.com>
2020-12-12 15:39:39 +0100

    #US614: first forms

--- frontend/libs/health-department/feature-case-detail/src/lib/event-new-dialog/event-new-dialog.component.ts
+++ frontend/libs/health-department/feature-case-detail/src/lib/event-new-dialog/event-new-dialog.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { SubSink } from 'subsink';
 
 @Component({
   selector: 'qro-event-new-dialog',
@@ -6,7 +9,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./event-new-dialog.component.scss'],
 })
 export class EventNewDialogComponent implements OnInit {
-  constructor() {}
+  subs = new SubSink();
+
+  eventFormGroup: FormGroup;
+
+  constructor(public dialogRef: MatDialogRef<EventNewDialogComponent>) {
+    this.subscribeToDialogCloseAndPassFormValueToParent();
+    this.initializeEventForm();
+  }
+
+  private subscribeToDialogCloseAndPassFormValueToParent() {
+    this.subs.add(
+      this.dialogRef.beforeClosed().subscribe(() => {
+        this.dialogRef.close(this.eventFormGroup.getRawValue());
+      })
+    );
+  }
+
+  private initializeEventForm() {
+    this.eventFormGroup = new FormGroup({
+      name: new FormControl(''),
+      date: new FormControl(''),
+      address: new FormControl(''),
+      status: new FormControl(''),
+      participants: new FormControl(''),
+      contactPerson: new FormControl(''),
+      description: new FormControl(''),
+      id: new FormControl(''),
+    });
+  }
 
   ngOnInit(): void {}
 }
