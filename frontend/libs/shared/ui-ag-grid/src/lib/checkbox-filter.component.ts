b74c2a65f8ec4622aced5b00b7c00806aa58eccc
VILLAN3LL3 <development@gutfrau.de>
2020-12-15 19:24:39 +0100

    CORE-667 fixed checkbox component after filter reset

--- frontend/libs/shared/ui-ag-grid/src/lib/checkbox-filter.component.ts
+++ frontend/libs/shared/ui-ag-grid/src/lib/checkbox-filter.component.ts
@@ -102,7 +102,7 @@ export class CheckboxFilterComponent implements IFilterAngularComp {
         }
       });
     } else {
-      this.options = [];
+      this.options = this.options || [];
     }
   }
 
