2ba32e59cf952c11971a386745aea927afac13ab
VILLAN3LL3 <development@gutfrau.de>
2020-12-16 22:16:59 +0100

    CORE-92 added basic form

--- frontend/libs/auth/feature-reset-password/src/lib/auth-feature-reset-password.module.ts
+++ frontend/libs/auth/feature-reset-password/src/lib/auth-feature-reset-password.module.ts
@@ -1,13 +1,21 @@
+import { SharedUiButtonModule } from '@qro/shared/ui-button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedUiMaterialModule } from '@qro/shared/ui-material';
 import { PasswordResetComponent } from './password-reset/password-reset.component';
 import { PasswordForgottenComponent } from './password-forgotten/password-forgotten.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { SharedUtilTranslationModule } from '@qro/shared/util-translation';
 
 @NgModule({
   imports: [
     CommonModule,
-
+    SharedUiMaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    SharedUiButtonModule,
+    SharedUtilTranslationModule,
     RouterModule.forChild([
       {
         path: '',
--- frontend/libs/auth/feature-reset-password/src/lib/password-forgotten/password-forgotten.component.ts
+++ frontend/libs/auth/feature-reset-password/src/lib/password-forgotten/password-forgotten.component.ts
@@ -1,4 +1,7 @@
+import { TrimmedPatternValidator, ValidationErrorService, VALIDATION_PATTERNS } from '@qro/shared/util-forms';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'qro-password-forgotten',
@@ -6,7 +9,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./password-forgotten.component.scss'],
 })
 export class PasswordForgottenComponent implements OnInit {
-  constructor() {}
+  formGroup: FormGroup;
+  loading = false;
 
-  ngOnInit() {}
+  constructor(public validationErrorService: ValidationErrorService, private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.createFormGroup();
+  }
+
+  createFormGroup() {
+    this.formGroup = this.formBuilder.group({
+      username: new FormControl(null, [
+        Validators.required,
+        Validators.minLength(1),
+        Validators.maxLength(30),
+        TrimmedPatternValidator.trimmedPattern(VALIDATION_PATTERNS.username),
+      ]),
+      email: new FormControl(null, [TrimmedPatternValidator.trimmedPattern(VALIDATION_PATTERNS.email)]),
+    });
+  }
+
+  trimValue(input: MatInput) {
+    input.value = input.value.trim();
+  }
+
+  submitForm() {}
 }
