857ab4f8cbf10ee439be456fd9763ca70c334784
bit-shube <stephen.hube@bridging-it.de>
2021-01-17 14:29:54 +0100

    S8: check contacts as GAMA

--- frontend/apps/quarano-frontend-e2e/src/integration/client/enrollment.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/client/enrollment.spec.ts
@@ -1,59 +1,5 @@
 /// <reference types="cypress" />
 
-describe('enrollment happy path', () => {
-  beforeEach(() => {
-    cy.server();
-    cy.route('POST', '/enrollment/completion?withoutEncounters=true').as('completeEnrollment');
-    cy.route('PUT', '/enrollment/questionnaire').as('updateQuestionnaire');
-    cy.route('PUT', '/enrollment/details').as('updatePersonalDetails');
-
-    cy.logInNotEnrolledClient();
-  });
-
-  describe('basic data', () => {
-    it('form completion', () => {
-      cy.location('pathname').should('eq', '/client/enrollment/basic-data');
-      cy.get('[data-cy="first-step-button"] button').should('be.disabled');
-      cy.get('[data-cy="street-input"] input[matInput]').type('Platz der Republik');
-      cy.get('[data-cy="house-number-input"] input[matInput]').type('1');
-      cy.get('[data-cy="zip-code-input"] input[matInput]').type('68163');
-      cy.get('[data-cy="city-input"] input[matInput]').type('Mannheim');
-      cy.get('[data-cy="first-step-button"] button').should('be.enabled');
-      cy.get('[data-cy="first-step-button"] button').click();
-      cy.wait('@updatePersonalDetails').its('status').should('eq', 200);
-      cy.get('[data-cy="second-step-button"] button').should('exist');
-    });
-  });
-
-  describe('questionnaire', () => {
-    it('form completion', () => {
-      cy.location('pathname').should('eq', '/client/enrollment/basic-data');
-      cy.get('[data-cy="second-step-button"] button').should('be.disabled');
-      cy.get('[data-cy="has-no-symptoms-option"]').click();
-      cy.get('[data-cy="has-no-pre-existing-conditions-option"]').click();
-      cy.get('[data-cy="no-medical-staff-option"]').click();
-      cy.get('[data-cy="no-contact-option"]').click();
-      cy.get('[data-cy="second-step-button"] button').should('be.enabled');
-      cy.get('[data-cy="second-step-button"] button').click();
-      cy.wait('@updateQuestionnaire').its('status').should('eq', 200);
-      cy.get('[data-cy="third-step-button"] button').should('exist');
-    });
-  });
-
-  describe('retrospective contacts', () => {
-    it('form completion', () => {
-      cy.location('pathname').should('eq', '/client/enrollment/basic-data');
-      cy.get('[data-cy="third-step-button"] button').should('be.enabled');
-      cy.get('[data-cy="third-step-button"] button').click();
-      cy.get('[data-cy="confirm-button"]').click();
-      cy.wait('@completeEnrollment').its('status').should('eq', 200);
-      cy.location('pathname').should('eq', '/client/diary/diary-list');
-      cy.get('[data-cy="diary-menu-item"]').should('exist');
-      cy.get('[data-cy="contact-person-menu-item"]').should('exist');
-    });
-  });
-});
-
 describe('enrollment external zip code', () => {
   beforeEach(() => {
     cy.server();
