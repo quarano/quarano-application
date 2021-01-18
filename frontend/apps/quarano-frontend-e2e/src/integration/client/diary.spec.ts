47f0839362f0f141030d197d0cc7196b7935f348
bit-shube <stephen.hube@bridging-it.de>
2021-01-18 19:13:27 +0100

    CORE-365 reactivate test

--- frontend/apps/quarano-frontend-e2e/src/integration/client/diary.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/client/diary.spec.ts
@@ -21,14 +21,13 @@ describe('Adding new symptom to symptom diary', () => {
     cy.get('[data-cy="cancel-diary-entry"]').should('be.enabled');
   });
 
-  it.skip('happy path', () => {
+  it('happy path', () => {
     cy.location('pathname').should('eq', '/client/diary/diary-list');
     cy.get('[data-cy="add-diary-entry"]').click();
     cy.get('[data-cy="add-missing-contacts"]').should('exist');
     cy.get('[data-cy="save-diary-entry"] button').should('be.disabled');
 
-    cy.get('[data-cy="body-temperature"]').trigger('mousedown', 'center', { button: 0 });
-    cy.wait(1000); // Seems to be necessary
+    cy.get('[data-cy="body-temperature"]').trigger('mousedown', 'center', { button: 0 }).click();
 
     cy.get('mat-slide-toggle').should('have.length', 8);
     cy.get('#mat-slide-toggle-1').click();
