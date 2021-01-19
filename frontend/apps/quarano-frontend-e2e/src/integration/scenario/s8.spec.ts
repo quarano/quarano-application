8056f2061411e5a9a7f1fe171f96478c654b6742
bit-shube <stephen.hube@bridging-it.de>
2021-01-19 20:48:49 +0100

    S8: remove console.log

--- frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
@@ -181,7 +181,6 @@ describe(
         .then(($body) => {
           expect($body.firstName).to.eq('Claire');
           expect($body.lastName).to.eq('Fraser');
-          console.log($body);
           expect($body._embedded.originCases[0].firstName).to.eq('Markus');
           expect($body._embedded.originCases[0].lastName).to.eq('Hanser');
         });
