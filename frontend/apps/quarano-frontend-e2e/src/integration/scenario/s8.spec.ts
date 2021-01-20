a9c5f9793541f7672ad75feb765391fc257db046
bit-shube <stephen.hube@bridging-it.de>
2021-01-20 21:52:28 +0100

    S8: add more assertions for personal data, split test into smaller parts

--- frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
@@ -8,7 +8,9 @@ describe(
   () => {
     before((done) => {
       cy.restartBackend(done);
+    });
 
+    beforeEach(() => {
       cy.intercept('POST', '/enrollment/completion?withoutEncounters=false').as('completeEnrollment');
       cy.intercept('PUT', '/enrollment/questionnaire').as('updateQuestionnaire');
       cy.intercept('PUT', '/enrollment/details').as('updatePersonalDetails');
@@ -19,169 +21,189 @@ describe(
       cy.intercept('GET', '/hd/cases/*/questionnaire').as('questionnaire');
     });
 
-    it('can complete enrollment and retrospective', () => {
-      cy.logInNotEnrolledClient();
+    describe('enroll as client', () => {
+      it('personal data', () => {
+        cy.logInNotEnrolledClient();
 
-      cy.location('pathname').should('eq', '/client/enrollment/basic-data');
+        cy.location('pathname').should('eq', '/client/enrollment/basic-data');
 
-      cy.get('[data-cy="first-step-button"] button').should('be.disabled');
-      cy.get('[data-cy="street-input"]').type('Hauptstraße');
-      cy.get('[data-cy="house-number-input"]').type('15');
-      cy.get('[data-cy="zip-code-input"]').type('68199');
-      cy.get('[data-cy="city-input"]').type('Mannheim');
-      cy.get('[data-cy="first-step-button"] button').click();
-      cy.wait('@updatePersonalDetails').its('response.statusCode').should('eq', 200);
+        cy.get('[data-cy="first-step-button"] button').should('be.disabled');
+        cy.get('[data-cy="personal-data-firstName"] input').should('have.value', 'Markus');
+        cy.get('[data-cy="personal-data-lastName"] input').should('have.value', 'Hanser');
+        cy.get('[data-cy="personal-data-dateOfBirth"] input').should('have.value', '1.1.1990');
+        cy.get('[data-cy="personal-data-email"] input').should('have.value', 'markus.hanser@testtest.de');
+        cy.get('[data-cy="personal-data-phone"] input').should('have.value', '0621222255');
+        cy.get('[data-cy="street-input"]').type('Hauptstraße');
+        cy.get('[data-cy="house-number-input"]').type('15');
+        cy.get('[data-cy="zip-code-input"]').type('68199');
+        cy.get('[data-cy="city-input"]').type('Mannheim');
+        cy.get('[data-cy="first-step-button"] button').click();
+        cy.wait('@updatePersonalDetails').its('response.statusCode').should('eq', 200);
+      });
 
-      cy.get('[data-cy="second-step-button"] button').should('be.disabled');
-      cy.get('[data-cy="has-symptoms-option"]').click();
+      it('initial questionnaire', () => {
+        cy.get('[data-cy="second-step-button"] button').should('be.disabled');
+        cy.get('[data-cy="has-symptoms-option"]').click();
 
-      const today = new Date();
-      const tenDaysAgo = new Date();
-      tenDaysAgo.setDate(today.getDate() - 10);
+        const today = new Date();
+        const tenDaysAgo = new Date();
+        tenDaysAgo.setDate(today.getDate() - 10);
 
-      cy.get('[data-cy="dayOfFirstSymptoms"]').type(tenDaysAgo.toLocaleDateString('de-DE'));
-      cy.get('[data-cy="characteristicSymptoms"]').type('Fever');
-      cy.get('mat-option').click();
-      cy.get('[data-cy="familyDoctor"]').type('Dr Schmidt');
-      cy.get('[data-cy="has-pre-existing-conditions-option"]').click();
-      cy.get('[data-cy="hasPreExistingConditionsDescription"]').type('test');
-      cy.get('[data-cy="medical-staff-option"]').click();
-      cy.get('[data-cy="belongToMedicalStaffDescription"]').type('Merck');
-      cy.get('[data-cy="contact-option"]').click();
-      cy.get('[data-cy="hasContactToVulnerablePeopleDescription"]').type('Peter Aalen');
-      cy.get('[data-cy="second-step-button"] button').click();
-      cy.wait('@updateQuestionnaire').its('response.statusCode').should('eq', 200);
+        cy.get('[data-cy="dayOfFirstSymptoms"]').type(tenDaysAgo.toLocaleDateString('de-DE'));
+        cy.get('[data-cy="characteristicSymptoms"]').type('Fever');
+        cy.get('mat-option').click();
+        cy.get('[data-cy="familyDoctor"]').type('Dr Schmidt');
+        cy.get('[data-cy="has-pre-existing-conditions-option"]').click();
+        cy.get('[data-cy="hasPreExistingConditionsDescription"]').type('test');
+        cy.get('[data-cy="medical-staff-option"]').click();
+        cy.get('[data-cy="belongToMedicalStaffDescription"]').type('Merck');
+        cy.get('[data-cy="contact-option"]').click();
+        cy.get('[data-cy="hasContactToVulnerablePeopleDescription"]').type('Peter Aalen');
+        cy.get('[data-cy="second-step-button"] button').click();
+        cy.wait('@updateQuestionnaire').its('response.statusCode').should('eq', 200);
+      });
 
-      cy.location('pathname').should('eq', '/client/enrollment/basic-data');
-      cy.get('mat-horizontal-stepper').find('fieldset').should('have.length.greaterThan', 13);
-      cy.get('[data-cy="multiple-auto-complete-input"]').eq(1).type('Claire Fraser').blur();
-      cy.get('[data-cy="confirm-button"]').click();
+      it('contacts', () => {
+        cy.location('pathname').should('eq', '/client/enrollment/basic-data');
+        cy.get('mat-horizontal-stepper').find('fieldset').should('have.length.greaterThan', 13);
+        cy.get('[data-cy="multiple-auto-complete-input"]').eq(1).type('Claire Fraser').blur();
+        cy.get('[data-cy="confirm-button"]').click();
 
-      cy.get('qro-contact-person-dialog').should('exist');
-      cy.get('[data-cy="contact-person-form-phone"] input')
-        .click()
-        .then(($elem) => {
-          cy.wrap($elem).type('123123123123');
-        });
-      cy.get('[data-cy="submit-button"] button').click();
+        cy.get('qro-contact-person-dialog').should('exist');
+        cy.get('[data-cy="contact-person-form-phone"] input')
+          .click()
+          .then(($elem) => {
+            cy.wrap($elem).type('123123123123');
+          });
+        cy.get('[data-cy="submit-button"] button').click();
 
-      cy.get('[data-cy="multiple-auto-complete-input"]').eq(3).type('Claire Fraser');
-      cy.get('mat-option').click();
+        cy.get('[data-cy="multiple-auto-complete-input"]').eq(3).type('Claire Fraser');
+        cy.get('mat-option').click();
 
-      cy.get('[data-cy="third-step-button"] button').click();
-      cy.wait('@completeEnrollment').its('response.statusCode').should('eq', 200);
-      cy.wait('@diary').its('response.statusCode').should('eq', 200);
+        cy.get('[data-cy="third-step-button"] button').click();
+        cy.wait('@completeEnrollment').its('response.statusCode').should('eq', 200);
+        cy.wait('@diary').its('response.statusCode').should('eq', 200);
+        cy.location('pathname').should('eq', '/client/diary/diary-list');
+        cy.get('[data-cy="diary-menu-item"]').should('exist');
+        cy.get('[data-cy="contact-person-menu-item"]').should('exist');
+      });
 
-      cy.location('pathname').should('eq', '/client/diary/diary-list');
+      it('check client profile page', () => {
+        cy.get('[data-cy="profile-user-button"]').click();
+        cy.get('[data-cy="profile-button"]').click();
+        cy.wait('@details').its('response.statusCode').should('eq', 200);
+        cy.location('pathname').should('include', '/client/profile');
 
-      cy.get('[data-cy="diary-menu-item"]').should('exist');
-      cy.get('[data-cy="contact-person-menu-item"]').should('exist');
+        cy.get('[data-cy="personal-data-firstName"] input').should('have.value', 'Markus');
+        cy.get('[data-cy="personal-data-lastName"] input').should('have.value', 'Hanser');
+        cy.get('[data-cy="personal-data-dateOfBirth"] input').should('have.value', '1.1.1990');
+        cy.get('[data-cy="personal-data-email"] input').should('have.value', 'markus.hanser@testtest.de');
+        cy.get('[data-cy="personal-data-phone"] input').should('have.value', '0621222255');
 
-      cy.get('[data-cy="profile-user-button"]').click();
-      cy.get('[data-cy="profile-button"]').click();
-      cy.wait('@details').its('response.statusCode').should('eq', 200);
-      cy.location('pathname').should('include', '/client/profile');
+        cy.get('[data-cy="street-input"] input').should('have.value', 'Hauptstraße');
+        cy.get('[data-cy="house-number-input"] input').should('have.value', '15');
+        cy.get('[data-cy="zip-code-input"] input').should('have.value', '68199');
+        cy.get('[data-cy="city-input"] input').should('have.value', 'Mannheim');
 
-      cy.get('[data-cy="personal-data-firstName"] input').should('have.value', 'Markus');
-      cy.get('[data-cy="personal-data-lastName"] input').should('have.value', 'Hanser');
-      cy.get('[data-cy="personal-data-dateOfBirth"] input').should('have.value', '1.1.1990');
-      cy.get('[data-cy="personal-data-email"] input').should('have.value', 'markus.hanser@testtest.de');
-      cy.get('[data-cy="personal-data-phone"] input').should('have.value', '0621222255');
+        cy.get('[data-cy="contact-person-menu-item"]').click();
+        cy.wait('@contacts').its('response.statusCode').should('eq', 200);
+        cy.location('pathname').should('eq', '/client/contact-persons/contact-person-list');
+        cy.get('mat-card').should('contain', 'Claire Fraser');
 
-      cy.get('[data-cy="street-input"] input').should('have.value', 'Hauptstraße');
-      cy.get('[data-cy="house-number-input"] input').should('have.value', '15');
-      cy.get('[data-cy="zip-code-input"] input').should('have.value', '68199');
-      cy.get('[data-cy="city-input"] input').should('have.value', 'Mannheim');
+        cy.logOut();
+      });
+    });
+    describe('check results GAMA', () => {
+      it('check edit page', () => {
+        cy.logInAgent();
 
-      cy.get('[data-cy="contact-person-menu-item"]').click();
-      cy.wait('@contacts').its('response.statusCode').should('eq', 200);
-      cy.location('pathname').should('eq', '/client/contact-persons/contact-person-list');
-      cy.get('mat-card').should('contain', 'Claire Fraser');
+        cy.location('pathname').should('eq', Cypress.env('index_cases_url'));
+        cy.get('[data-cy="search-case-input"]').type('Markus');
+        cy.get('[data-cy="case-data-table"]').find('.ag-center-cols-container > .ag-row').should('have.length', 1);
+        cy.get('[data-cy="case-data-table"]')
+          .find('.ag-center-cols-container > .ag-row')
+          .then(($elems) => {
+            $elems[0].click();
+          });
+        cy.location('pathname').should('include', '/edit');
 
-      cy.logOut();
+        cy.get('.mat-tab-links').children().should('have.length', 6);
 
-      cy.logInAgent();
+        cy.wait('@case').its('response.statusCode').should('eq', 200);
+        cy.get('@case')
+          .its('response.body')
+          .then(($body) => {
+            expect($body.caseId).to.not.eq(null);
+            expect($body.caseId).to.not.eq('');
+          });
 
-      cy.location('pathname').should('eq', Cypress.env('index_cases_url'));
-      cy.get('[data-cy="search-case-input"]').type('Markus');
-      cy.get('[data-cy="case-data-table"]').find('.ag-center-cols-container > .ag-row').should('have.length', 1);
-      cy.get('[data-cy="case-data-table"]')
-        .find('.ag-center-cols-container > .ag-row')
-        .then(($elems) => {
-          $elems[0].click();
-        });
-      cy.location('pathname').should('include', '/edit');
+        cy.get('[data-cy="input-firstname"] input').should('have.value', 'Markus');
+        cy.get('[data-cy="input-lastname"] input').should('have.value', 'Hanser');
+        cy.get('[data-cy="input-dayofbirth"] input').should('have.value', '1.1.1990');
+        cy.get('[data-cy="input-email"] input').should('have.value', 'markus.hanser@testtest.de');
+        cy.get('[data-cy="input-phone"] input').should('have.value', '0621222255');
 
-      cy.get('.mat-tab-links').children().should('have.length', 6);
+        cy.get('[data-cy="street-input"] input').should('have.value', 'Hauptstraße');
+        cy.get('[data-cy="house-number-input"] input').should('have.value', '15');
+        cy.get('[data-cy="input-zipcode"] input').should('have.value', '68199');
+        cy.get('[data-cy="city-input"] input').should('have.value', 'Mannheim');
 
-      cy.wait('@case').its('response.statusCode').should('eq', 200);
-      cy.get('@case')
-        .its('response.body')
-        .then(($body) => {
-          expect($body.caseId).to.not.eq(null);
-          expect($body.caseId).to.not.eq('');
-        });
+        const today = new Date();
+        const tenDaysAgo = new Date();
+        tenDaysAgo.setDate(today.getDate() - 10);
 
-      cy.get('[data-cy="input-firstname"] input').should('have.value', 'Markus');
-      cy.get('[data-cy="input-lastname"] input').should('have.value', 'Hanser');
-      cy.get('[data-cy="input-dayofbirth"] input').should('have.value', '1.1.1990');
-      cy.get('[data-cy="input-email"] input').should('have.value', 'markus.hanser@testtest.de');
-      cy.get('[data-cy="input-phone"] input').should('have.value', '0621222255');
+        const twoDaysAgo = new Date();
+        twoDaysAgo.setDate(today.getDate() - 2);
+        const twoWeeksFromNow = new Date();
+        twoWeeksFromNow.setDate(today.getDate() + 14);
 
-      cy.get('[data-cy="street-input"] input').should('have.value', 'Hauptstraße');
-      cy.get('[data-cy="house-number-input"] input').should('have.value', '15');
-      cy.get('[data-cy="input-zipcode"] input').should('have.value', '68199');
-      cy.get('[data-cy="city-input"] input').should('have.value', 'Mannheim');
+        cy.get('[data-cy="input-testdate"] input').should('have.value', twoDaysAgo.toLocaleDateString('de'));
+        cy.get('[data-cy="input-quarantinestart"] input').should('have.value', today.toLocaleDateString('de'));
+        cy.get('[data-cy="input-quarantineend"]')
+          .find('input')
+          .should('have.value', twoWeeksFromNow.toLocaleDateString('de'));
+      });
+      it('check contacts', () => {
+        cy.get('[data-cy="contacts-tab"]').click();
+        cy.location('pathname').should('include', '/contacts');
+        cy.wait('@contacts').its('response.statusCode').should('eq', 200);
+        cy.get('@contacts')
+          .its('response.body')
+          .then((body) => {
+            expect(body._embedded.contacts[0].firstName).to.eq('Claire');
+            expect(body._embedded.contacts[0].lastName).to.eq('Fraser');
+          });
+        cy.get('[data-cy="case-data-table"]').find('.ag-center-cols-container > .ag-row').should('have.length', 1);
+      });
+      it('check questionnaire', () => {
+        cy.get('[data-cy="questionnaire-tab"]').click();
+        cy.location('pathname').should('include', '/questionnaire');
+        cy.wait('@questionnaire').its('response.statusCode').should('eq', 200);
 
-      const twoDaysAgo = new Date();
-      twoDaysAgo.setDate(today.getDate() - 2);
-      const twoWeeksFromNow = new Date();
-      twoWeeksFromNow.setDate(today.getDate() + 14);
+        // cy.get('[data-cy="symptoms-date"]').should('have.text', tenDaysAgo.toLocaleDateString()); TODO: check once dayjs is merged
+        cy.get('[data-cy="familyDoctor"]').should('have.text', 'Dr Schmidt');
+        cy.get('[data-cy="presumed-origin"]').should('have.text', 'Nicht angegeben');
+        cy.get('[data-cy="presumed-date"]').should('have.text', 'Nicht angegeben');
+        cy.get('[data-cy="pre-existing-conditions"]').should('have.text', ' test '); // TODO: spaces
+        cy.get('[data-cy="belongToMedicalStaffDescription"]').should('have.text', 'Merck');
+        cy.get('[data-cy="hasContactToVulnerablePeopleDescription"]').should('have.text', ' Peter Aalen '); // TODO: spaces
+      });
+      it('check associated contact', () => {
+        cy.get('[data-cy="contact-cases"]').click();
 
-      cy.get('[data-cy="input-testdate"] input').should('have.value', twoDaysAgo.toLocaleDateString('de'));
-      cy.get('[data-cy="input-quarantinestart"] input').should('have.value', today.toLocaleDateString('de'));
-      cy.get('[data-cy="input-quarantineend"]')
-        .find('input')
-        .should('have.value', twoWeeksFromNow.toLocaleDateString('de'));
+        cy.location('pathname').should('eq', Cypress.env('contact_cases_url'));
 
-      cy.get('[data-cy="contacts-tab"]').click();
-      cy.location('pathname').should('include', '/contacts');
-      cy.wait('@contacts').its('response.statusCode').should('eq', 200);
-      cy.get('@contacts')
-        .its('response.body')
-        .then((body) => {
-          cy.log(JSON.stringify(body));
-          expect(body[0].firstName).to.eq('Claire');
-          expect(body[0].lastName).to.eq('Fraser');
-        });
-      cy.get('[data-cy="case-data-table"]').find('.ag-center-cols-container > .ag-row').should('have.length', 1);
-
-      cy.get('[data-cy="questionnaire-tab"]').click();
-      cy.location('pathname').should('include', '/questionnaire');
-      cy.wait('@questionnaire').its('response.statusCode').should('eq', 200);
-
-      // cy.get('[data-cy="symptoms-date"]').should('have.text', tenDaysAgo.toLocaleDateString()); TODO: check once dayjs is merged
-      cy.get('[data-cy="familyDoctor"]').should('have.text', 'Dr Schmidt');
-      cy.get('[data-cy="presumed-origin"]').should('have.text', 'Nicht angegeben');
-      cy.get('[data-cy="presumed-date"]').should('have.text', 'Nicht angegeben');
-      cy.get('[data-cy="pre-existing-conditions"]').should('have.text', ' test '); // TODO: spaces
-      cy.get('[data-cy="belongToMedicalStaffDescription"]').should('have.text', 'Merck');
-      cy.get('[data-cy="hasContactToVulnerablePeopleDescription"]').should('have.text', ' Peter Aalen '); // TODO: spaces
-
-      cy.get('[data-cy="contact-cases"]').click();
-
-      cy.location('pathname').should('eq', Cypress.env('contact_cases_url'));
-
-      cy.get('[data-cy="search-case-input"] input').type('Claire');
-      cy.get('[data-cy="case-data-table"]').find('.ag-center-cols-container > .ag-row').eq(0).click();
-      cy.location('pathname').should('include', '/edit');
-      cy.wait('@case').its('response.statusCode').should('eq', 200);
-      cy.get('@case')
-        .its('response.body')
-        .then(($body) => {
-          expect($body._embedded.originCases[0].firstName).to.eq('Markus');
-          expect($body._embedded.originCases[0].lastName).to.eq('Hanser');
-        });
+        cy.get('[data-cy="search-case-input"] input').type('Claire');
+        cy.get('[data-cy="case-data-table"]').find('.ag-center-cols-container > .ag-row').eq(0).click();
+        cy.location('pathname').should('include', '/edit');
+        cy.wait('@case').its('response.statusCode').should('eq', 200);
+        cy.get('@case')
+          .its('response.body')
+          .then(($body) => {
+            expect($body._embedded.originCases[0].firstName).to.eq('Markus');
+            expect($body._embedded.originCases[0].lastName).to.eq('Hanser');
+          });
+      });
     });
   }
 );
