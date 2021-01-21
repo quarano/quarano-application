6aa0bfa248aa647c35059a8982f6710449f4cbac
bit-shube <stephen.hube@bridging-it.de>
2021-01-21 19:19:22 +0100

    S9: remove obsolete e2e

--- frontend/apps/quarano-frontend-e2e/src/integration/health-department/contact-cases/contact-cases.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/health-department/contact-cases/contact-cases.spec.ts
@@ -33,57 +33,6 @@ describe('health-department contact cases', () => {
       cy.get('[data-cy="client-cancel-button"]').should('be.enabled');
       cy.get('simple-snack-bar').should('exist');
     });
-
-    it('happy path', () => {
-      cy.location('pathname').should('eq', Cypress.env('index_cases_url'));
-      cy.get('[data-cy="contact-cases"]').should('exist');
-      cy.get('[data-cy="contact-cases"]').click();
-      cy.location('pathname').should('eq', Cypress.env('contact_cases_url'));
-      cy.get('[data-cy="case-data-table"]').should('exist');
-      cy.get('[data-cy="search-contact-case-input"]').type('Baum');
-      cy.get('[data-cy="case-data-table"]')
-        .find('.ag-center-cols-container > .ag-row')
-        .should('have.length', 1)
-        .eq(0)
-        .click();
-
-      cy.location('pathname').should('include', Cypress.env('health_department_url') + 'case-detail/contact/');
-
-      cy.get('[data-cy="input-field-test-date"]').type('5.6.2020').blur();
-      cy.get('[data-cy="covid-test-result-true"]').click();
-
-      cy.get('[data-cy="confirm-button"]').click();
-
-      cy.get('simple-snack-bar button').click();
-
-      cy.get('[data-cy="quarantine-start-input"]').type('6.6.2020').blur();
-      cy.get('[data-cy="quarantine-end-input"]').type('20.6.2020').blur();
-      cy.get('[data-cy="phone-number-input"]').type('0721123456').blur();
-
-      cy.get('[data-cy="client-submit-button"] button').should('be.enabled');
-      cy.get('[data-cy="client-submit-and-close-button"] button').should('be.enabled');
-      cy.get('[data-cy="client-cancel-button"]').should('be.enabled');
-
-      cy.get('[data-cy="zip-code-input"]').type('68163');
-      cy.get('[data-cy="street-input"]').type('Test');
-      cy.get('[data-cy="house-number-input"]').type('1');
-      cy.get('[data-cy="city-input"]').type('Mannheim');
-
-      cy.get('[data-cy="client-submit-button"] button').click();
-      cy.wait('@updateIndexCase').its('status').should('eq', 200);
-      cy.get('@updateIndexCase')
-        .its('response.body')
-        .then((body) => {
-          const caseId = body.caseId;
-          expect(caseId).not.to.eq(null);
-          expect(caseId).not.to.eq('');
-
-          cy.location('pathname').should(
-            'eq',
-            Cypress.env('health_department_url') + 'case-detail/index/' + caseId + '/edit'
-          );
-        });
-    });
   });
 
   describe('create new contact case', () => {
