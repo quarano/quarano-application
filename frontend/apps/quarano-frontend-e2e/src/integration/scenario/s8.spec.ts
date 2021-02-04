e2d1c6e98f6cc976c4581276a60276c3fa766fba
bit-shube <stephen.hube@bridging-it.de>
2021-02-04 17:00:27 +0100

    S8 reactivate assertion

--- frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
@@ -211,7 +211,7 @@ describe(
             expect($body._embedded.originCases[0].firstName).to.eq('Markus');
             expect($body._embedded.originCases[0].lastName).to.eq('Hanser');
           });
-        // cy.get('[data-cy="lazy-autocomplete-chip-list"]').contains('Hanser, Markus');
+        cy.get('[data-cy="lazy-autocomplete-chip-list"]').contains('Hanser, Markus');
       });
     });
   }
