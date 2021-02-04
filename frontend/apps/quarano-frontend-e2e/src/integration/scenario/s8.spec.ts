427d98c8f4d473113d73e69669058f0de30a22ea
bit-shube <stephen.hube@bridging-it.de>
2021-02-04 19:44:17 +0100

    S8: check path update to new cypress version use chrome instead of firefox on server

--- frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s8.spec.ts
@@ -211,6 +211,7 @@ describe(
             expect($body._embedded.originCases[0].firstName).to.eq('Markus');
             expect($body._embedded.originCases[0].lastName).to.eq('Hanser');
           });
+        cy.location('pathname').should('include', '/edit');
         cy.get('[data-cy="lazy-autocomplete-chip-list"]').contains('Hanser, Markus');
       });
     });
