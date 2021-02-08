e9f113b5bccc3ae341f63b19aa4ffb6aa5cdd4e2
VILLAN3LL3 <39371377+VILLAN3LL3@users.noreply.github.com>
2021-02-08 19:36:52 +0100

    CORE-694 Renamed alert (#199)
    * CORE-694 Renamed alert

--- frontend/libs/health-department/ui-action-alert/src/lib/action-alert/action-alert.component.ts
+++ frontend/libs/health-department/ui-action-alert/src/lib/action-alert/action-alert.component.ts
@@ -14,7 +14,10 @@ export class ActionAlertComponent implements ICellRendererAngularComp {
   agInit(params: any): void {
     this.params = params;
     params?.value.forEach((element) => {
-      this.alerts.push(this.alertConfigurationFor(element));
+      const config = this.alertConfigurationFor(element);
+      if (config) {
+        this.alerts.push(this.alertConfigurationFor(element));
+      }
     });
   }
 
@@ -23,6 +26,10 @@ export class ActionAlertComponent implements ICellRendererAngularComp {
   }
 
   private alertConfigurationFor(alert: Alert) {
-    return getAlertConfigurations().find((c) => c.alert === alert);
+    const config = getAlertConfigurations().find((c) => c.alert === alert);
+    if (!config) {
+      console.warn(`No alert configuration found for alert '${alert}'. Alert will be ignored`);
+    }
+    return config;
   }
 }
