514650ddbd82f61ba12c17855772cacb60f8fd1d
ma-xbo <38072209+ma-xbo@users.noreply.github.com>
2021-03-31 11:20:27 +0200

    CORE-675 e2e/s6 finished part 'add new contact for index'

--- frontend/apps/quarano-frontend-e2e/src/integration/scenario/s6.spec.ts
+++ frontend/apps/quarano-frontend-e2e/src/integration/scenario/s6.spec.ts
@@ -1,12 +1,5 @@
 /// <reference types="cypress" />
 
-import * as dayjs from 'dayjs';
-import 'dayjs/locale/de';
-import * as localeData from 'dayjs/plugin/localeData';
-
-dayjs.locale('de');
-dayjs.extend(localeData);
-
 /* Vorbedingung 1: GAMA hat Indexfall "Markus Hanser” mit Minimaldaten angelegt (S2) */
 // Es wurde ein neuer User für den Test angelegt
 // UUID: caaf631e-c7ba-495f-9a54-77e90a71100f
@@ -18,11 +11,11 @@ dayjs.extend(localeData);
 // Straße: Blütenweg 24
 // Ort: 68199 Mannheim
 // Initialer Fragebogen:
-//    - Hausarzt: Dr. Maier - Bergstraße 34, Mannheim
+//    - Hausarzt: Prof. Dr. Maier - Bergstraße 34, Mannheim
 //    - keine Symptome festgestellt
 //    - keine Vorerkrankungen
-//    - nicht mi
-//    - kein Kontakt zu Risikopersonen
+//    - Origin of Infection: Schnitzel Wettessen in der Wirtschaft Zum blauen Schwert
+//    - Kontakt zu Risikopersonen: Alterheim Ruhetstill
 // siehe: backend/src/main/java/quarano/tracking/TrackedPersonDataInitializer.java
 
 /* Vorbedingung 2: Bürger hat sich registriert, die Initiale Datenerfassung abgeschlossen und Kontakte sind angelegt (Claire Fraser, Peter Aalen) (S8) */
@@ -50,11 +43,14 @@ describe(
 
     beforeEach(() => {
       cy.intercept('GET', '/enrollment').as('getEnrollment');
+      cy.intercept('POST', '/contacts').as('postContacts');
+      cy.intercept('GET', '/contacts').as('getContacts');
+      cy.intercept('POST', '/diary').as('postDiary');
     });
 
     describe('enroll as client', () => {
       it('add new contact for index', () => {
-        /* 1 - Login als Bürger Thorsten Mehler Login: test8:test123 */
+        /* 1 - Login als Bürger Thorsten Mehler (“test8”, “test123”) */
         cy.logIn('test8', 'test123');
 
         cy.wait('@getEnrollment').its('response.statusCode').should('eq', 200);
@@ -75,22 +71,63 @@ describe(
         cy.get('[data-cy="add-missing-contacts"]').should('exist').click();
 
         /* 6 - Vorname: -> "Conny" */
+        cy.get('[data-cy="contact-person-form-firstName"]').should('exist').type('Conny');
+
         /* 7 - Nachname: -> "Hügel" */
+        cy.get('[data-cy="contact-person-form-lastName"]').should('exist').type('Hügel');
+
         /* 8 - Telefonnummer: -> "0621486375" */
+        cy.get('[data-cy="contact-person-form-phone"]').should('exist').type('0621486375');
+
         /* 9 - Mail-Adresse: -> "chuegel@web.de" */
+        cy.get('[data-cy="contact-person-form-email"]').should('exist').type('chuegel@web.de');
+
         /* 10 - Straße: -> "Sonderweg" */
+        cy.get('[data-cy="contact-person-form-street"]').should('exist').type('Sonderweg');
+
         /* 11 - Hausnummer: -> "429" */
+        cy.get('[data-cy="contact-person-form-houseNumber"]').should('exist').type('429');
+
         /* 12 - Postleitzahl: -> "68239" */
-        /* 13 - Ort: -> "Mannheim"14 - Risikoeinschätzung: arbeitet im medizinischen Umfeld: -> "ja" */
+        cy.get('[data-cy="contact-person-form-zipCode"]').should('exist').type('68239');
+
+        /* 13 - Ort: -> "Mannheim" */
+        cy.get('[data-cy="contact-person-form-city"]').should('exist').type('Mannheim');
+
+        /* 14 - Risikoeinschätzung: arbeitet im medizinischen Umfeld: -> "ja" */
+        cy.get('[data-cy="contact-person-form-isHealthStaff-true"]').should('exist').click();
+
         /* 15 - Risikoeinschätzung: ist über 60: -> "ja" */
+        cy.get('[data-cy="contact-person-form-isSenior-true"]').should('exist').click();
+
         /* 16 - Risikoeinschätzung: bekannt Vorerkrankungen: -> "nein" */
+        cy.get('[data-cy="contact-person-form-hasPreExistingConditions-false"]').should('exist').click();
+
+        /* 16b - Klicke Speichern */
+        cy.get('[data-cy="submit-button"]').should('exist').click();
+        cy.wait('@postContacts').its('response.statusCode').should('eq', 201);
+
+        /* 16c - Klicke Speichern */
+        cy.get('[data-cy="save-diary-entry"]').should('exist').click();
+        cy.wait('@postDiary').its('response.statusCode').should('eq', 201);
+
         /* CHECK: Bürger sieht alle Kontakte (Leon Duerr, Anna Beike und Conny Hügel) unter der Ansicht "Kontaktpersonen" */
+        cy.get('[data-cy="contact-person-menu-item"]').should('exist').click();
+        cy.wait('@getContacts').its('response.statusCode').should('eq', 200);
+
+        cy.get('[data-cy="contact-person-list-name"]')
+          .should('exist')
+          .each(($el) => {
+            expect($el.text()).to.be.oneOf(['Leon Duerr', 'Anna Beike', 'Conny Hügel']);
+          });
+
         /* 17 - Logout als Bürger */
+        cy.logOut();
       });
 
       it('test for health department user', () => {
         /* 18 - Login als GAMA "agent1" */
-        /* CHECK: Unter dem Reiter "Kontaktpersonen" sind alle Kontakte (Jack Randel, Claire Fraser, Roger Fraser und Conny Hügel) für GAMA sichtbar und mit Status "angelegt" erfasst */
+        /* CHECK: Unter dem Reiter "Kontaktpersonen" sind alle Kontakte (Claire Fraser, Roger Fraser und Conny Hügel) für GAMA sichtbar und mit Status "angelegt" erfasst */
         /* CHECK: für alle Kontakte (Leon Duerr, Anna Beike und Conny Hügel) ist der Ursprungsfall "Markus Hanser" in der Übersicht hinterlegt */
         /* 19 - suche unter dem Reiter "Indexfälle" "Markus Hanser" */
         /* CHECK: Unter "Kontakte" sind alle alten und neu angelegten Kontakte (Leon Duerr, Anna Beike und Conny Hügel) mit der dazugehörigen Risikoeinstufung  (für Conny Hügel erfasst) sichtbar */
