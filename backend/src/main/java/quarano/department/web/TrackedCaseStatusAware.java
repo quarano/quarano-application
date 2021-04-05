220b10ca257da735cfc7497d9d1ea04af2526724
David <david.bauknecht@posteo.de>
2021-04-05 11:16:38 +0200

    CORE-455: review findings

--- backend/src/main/java/quarano/department/web/TrackedCaseStatusAware.java
+++ backend/src/main/java/quarano/department/web/TrackedCaseStatusAware.java
@@ -42,8 +42,8 @@ public class TrackedCaseStatusAware<T extends RepresentationModel<T>> extends Re
 				.and(MvcLink.of(controller.getDiaryOfCase(caseId, null), DIARY))
 				.and(MvcLink.of(controller.getCase(caseId, null), IanaLinkRelations.SELF))
 				.andIf(trackedCase.isIndexCase(), MvcLink.of(controller.getContactsOfCase(caseId, null), CONTACTS))
-				.andIf(trackedCase.getTrackedPerson().getAccount().isEmpty(), () -> MvcLink.of(registrationController.createRegistration(caseId, null), RENEW))
-				.andIf(trackedCase.getTrackedPerson().getAccount().isPresent(),MvcLink.of(registrationController.removeAccountOfTrackedPerson(caseId, null), ACCOUNT))
+				.andIf((trackedCase.getTrackedPerson().getAccount().isEmpty() && !trackedCase.getStatus().equals(TrackedCase.Status.OPEN)), () -> MvcLink.of(registrationController.createRegistration(caseId, null), RENEW))
+				.andIf(trackedCase.getTrackedPerson().getAccount().isPresent(), MvcLink.of(registrationController.removeAccountOfTrackedPerson(caseId, null), ACCOUNT))
 				.andIf(trackedCase.isEligibleForTracking(), () -> MvcLink.of(registrationController.createRegistration(caseId, null), START_TRACKING))
 				.andIf(trackedCase.hasQuestionnaire(),
 						() -> MvcLink.of(controller.getQuestionnaire(caseId, null), QUESTIONNAIRE))
